2be2d59fb61aa45d4309ede37631cac3477d61a3
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2022-09-23 17:52:47 +0900

    Create baekjoon.java
    25591

---
+++ baekjoon.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class baekjoon {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		int a = 100 - x;
+		int b = 100 - y;
+		int c = 100 - a - b;
+		int d = a * b;
+		int q = d / 100;
+		int r = d % 100;
+		System.out.println(a + " " + b + " " + c + " " + d + " " + q + " " + r);
+		System.out.println((c+q) + " " + r);
+	}
+
+}
