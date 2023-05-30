a3631f1ae56109c6d5ae3c3d5a1206bd5fd75467
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-05-31 00:02:57 +0900

    11720

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		String s = scanner.next();
+		char[] c = s.toCharArray();
+		
+		int sum = 0;
+		for(int i=0;i<n;i++)
+			sum += c[i] - '0';
+		System.out.println(sum);
+		scanner.close();
 	}
 
 }
