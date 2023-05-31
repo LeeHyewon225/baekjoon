d20c40e22826750aeddcc9d7b359dfd2e92eba02
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-05-31 17:41:31 +0900

    10986번 - 나머지 합

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,30 +1,25 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-		int N = Integer.parseInt(stringTokenizer.nextToken());
-		int M = Integer.parseInt(stringTokenizer.nextToken());
-		long S[][] = new long[N + 1][N + 1];
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int M = s.nextInt();
+		long S[] = new long[N + 1];
+		long C[] = new long[M];
+		long result = 0;
 		for (int i = 1; i < N + 1; i++) {
-			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-			for (int j = 1; j < N + 1; j++) {
-				int a = Integer.parseInt(stringTokenizer.nextToken());
-				S[i][j] = S[i][j - 1] + S[i - 1][j] - S[i - 1][j - 1] + a;
-			}
+			int a = s.nextInt();
+			S[i] += S[i - 1] + a;
+			S[i] %= M;
+			if (S[i] == 0)
+				result++;
+			C[(int) S[i]]++;
 		}
 		for (int i = 0; i < M; i++) {
-			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-			int x1 = Integer.parseInt(stringTokenizer.nextToken());
-			int y1 = Integer.parseInt(stringTokenizer.nextToken());
-			int x2 = Integer.parseInt(stringTokenizer.nextToken());
-			int y2 = Integer.parseInt(stringTokenizer.nextToken());
-			System.out.println(S[x2][y2] - S[x2][y1 - 1] - S[x1 - 1][y2] + S[x1 - 1][y1 - 1]);
+			result += C[i] * (C[i] - 1) / 2;
 		}
+		System.out.println(result);
 	}
 }
