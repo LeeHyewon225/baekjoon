b8d72ffe0f8b212b45d55052d89eb8f8bfa32466
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-05-31 16:16:43 +0900

    11660번 - 구간 합 구하기 5

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,25 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		int M = scanner.nextInt();
-		long S[] = new long[N + 1];
-		S[0] = 0;
-		int sum = 0;
-		for(int i=0;i<N;i++) {
-			int a = scanner.nextInt();
-			sum += a;
-			S[i + 1] = sum;
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		int N = Integer.parseInt(stringTokenizer.nextToken());
+		int M = Integer.parseInt(stringTokenizer.nextToken());
+		long S[][] = new long[N + 1][N + 1];
+		for (int i = 1; i < N + 1; i++) {
+			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			for (int j = 1; j < N + 1; j++) {
+				int a = Integer.parseInt(stringTokenizer.nextToken());
+				S[i][j] = S[i][j - 1] + S[i - 1][j] - S[i - 1][j - 1] + a;
+			}
 		}
-		for(int i=0;i<M;i++) {
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
-			System.out.println(S[b] - S[a - 1]);
+		for (int i = 0; i < M; i++) {
+			stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+			int x1 = Integer.parseInt(stringTokenizer.nextToken());
+			int y1 = Integer.parseInt(stringTokenizer.nextToken());
+			int x2 = Integer.parseInt(stringTokenizer.nextToken());
+			int y2 = Integer.parseInt(stringTokenizer.nextToken());
+			System.out.println(S[x2][y2] - S[x2][y1 - 1] - S[x1 - 1][y2] + S[x1 - 1][y1 - 1]);
 		}
-		scanner.close();
 	}
- 
 }
