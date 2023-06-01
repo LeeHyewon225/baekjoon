9d479b78d3ac6241f1bc5e0eea7122d85ebf6d2a
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-01 17:02:32 +0900

    12891번 - DNA 비밀번호

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -6,35 +6,71 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int check_list[] = new int[4];
+	static int window[] = new int[4];
+	static int count = 0;
 
+	private static void add(char c) {
+		switch (c) {
+		case 'A':
+			window[0]++;
+			break;
+		case 'C':
+			window[1]++;
+			break;
+		case 'G':
+			window[2]++;
+			break;
+		case 'T':
+			window[3]++;
+			break;
+		}
+	}
+	
+	private static void remove(char c) {
+		switch (c) {
+		case 'A':
+			window[0]--;
+			break;
+		case 'C':
+			window[1]--;
+			break;
+		case 'G':
+			window[2]--;
+			break;
+		case 'T':
+			window[3]--;
+			break;
+		}
+	}
+	
+	private static void check() {
+		count++;
+		for(int j=0;j<4;j++) {
+			if(window[j]<check_list[j]) {
+				count--;
+				break;
+			}
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
-		long A[] = new long[N];
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		for (int i = 0; i < N; i++)
-			A[i] = Long.parseLong(st.nextToken());
-		Arrays.sort(A);
-		int count = 0;
-		for (int k = 0; k < N; k++) {
-			int i = 0;
-			int j = N - 1;
-			while (i < j) {
-				if (A[i] + A[j] < A[k])
-					i++;
-				else if (A[i] + A[j] > A[k])
-					j--;
-				else {
-					if (i != k && j != k) {
-						count++;
-						break;
-					}
-					else if(i==k)
-						i++;
-					else
-						j--;
-				}
-			}
+		int S = Integer.parseInt(st.nextToken());
+		int P = Integer.parseInt(st.nextToken());
+		char A[] = bf.readLine().toCharArray();
+		st = new StringTokenizer(bf.readLine());
+		for (int i = 0; i < 4; i++)
+			check_list[i] = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < P; i++) 
+			add(A[i]);
+		check();		
+		for (int i = 0; i < S - P; i++) {
+			remove(A[i]);
+			add(A[i+P]);
+			check();
 		}
 		System.out.println(count);
 		bf.close();
