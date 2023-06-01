d51a7b532ed290aefec83b05d5f357d6caa6b113
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-01 15:38:18 +0900

    1940번 - 주몽

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,29 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int N = s.nextInt();
-		int sum = 1;
-		int count = 1;
-		int start_index = 1;
-		int end_index = 1;
-		while(start_index < (N+1)/2){
-			if(sum < N) {
-				end_index++;
-				sum += end_index;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+
+		int A[] = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			A[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(A);
+		
+		int start=0;
+		int end = N-1;
+		int count =0;
+		
+		while(start<end) {
+			if(A[start] + A[end] == M) {
+				count++;
+				start++;
+				end--;
 			}
-			else if(sum > N) {
-				sum -= start_index;
-				start_index++;
+			else if(A[start] + A[end] < M) {
+				start++;
 			}
 			else {
-				count++;
-				end_index++;
-				sum += end_index;
+				end--;
 			}
 		}
 		System.out.println(count);
+		br.close();
 	}
 }
