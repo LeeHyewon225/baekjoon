47698e561f1230223877084a2e12617ab8a261eb
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-01 18:10:08 +0900

    11003번 - 최솟값 찾기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,78 +1,45 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int check_list[] = new int[4];
-	static int window[] = new int[4];
-	static int count = 0;
 
-	private static void add(char c) {
-		switch (c) {
-		case 'A':
-			window[0]++;
-			break;
-		case 'C':
-			window[1]++;
-			break;
-		case 'G':
-			window[2]++;
-			break;
-		case 'T':
-			window[3]++;
-			break;
+	static class Node {
+		public int value;
+		public int index;
+
+		Node(int value, int index) {
+			this.value = value;
+			this.index = index;
 		}
 	}
-	
-	private static void remove(char c) {
-		switch (c) {
-		case 'A':
-			window[0]--;
-			break;
-		case 'C':
-			window[1]--;
-			break;
-		case 'G':
-			window[2]--;
-			break;
-		case 'T':
-			window[3]--;
-			break;
-		}
-	}
-	
-	private static void check() {
-		count++;
-		for(int j=0;j<4;j++) {
-			if(window[j]<check_list[j]) {
-				count--;
-				break;
-			}
-		}
-	}
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int S = Integer.parseInt(st.nextToken());
-		int P = Integer.parseInt(st.nextToken());
-		char A[] = bf.readLine().toCharArray();
+		int N = Integer.parseInt(st.nextToken());
+		int L = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(bf.readLine());
-		for (int i = 0; i < 4; i++)
-			check_list[i] = Integer.parseInt(st.nextToken());
-		for (int i = 0; i < P; i++) 
-			add(A[i]);
-		check();		
-		for (int i = 0; i < S - P; i++) {
-			remove(A[i]);
-			add(A[i+P]);
-			check();
+		int A[] = new int[N];
+		Deque<Node> deque = new LinkedList<>();
+		for (int i = 0; i < N; i++) {
+			int now = Integer.parseInt(st.nextToken());
+			while (!deque.isEmpty() && deque.getLast().value > now)
+				deque.removeLast();
+			deque.addLast(new Node(now, i));
+			if (deque.getFirst().index < i - L + 1)
+				deque.removeFirst();
+			bw.write(deque.getFirst().value + " ");
 		}
-		System.out.println(count);
-		bf.close();
+		bw.flush();
+		bw.close();
 	}
 }
