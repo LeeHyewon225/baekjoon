4a95ea12813a303bc794690ec00584b1f084a0ef
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-02 15:25:16 +0900

    1874번 - 스택 수열

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,39 +7,44 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Scanner;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static class Node {
-		public int value;
-		public int index;
-
-		Node(int value, int index) {
-			this.value = value;
-			this.index = index;
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int L = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(bf.readLine());
-		int A[] = new int[N];
-		Deque<Node> deque = new LinkedList<>();
-		for (int i = 0; i < N; i++) {
-			int now = Integer.parseInt(st.nextToken());
-			while (!deque.isEmpty() && deque.getLast().value > now)
-				deque.removeLast();
-			deque.addLast(new Node(now, i));
-			if (deque.getFirst().index < i - L + 1)
-				deque.removeFirst();
-			bw.write(deque.getFirst().value + " ");
+		Scanner s = new Scanner(System.in);
+		StringBuffer sb = new StringBuffer();
+		Stack<Integer> stack = new Stack<>();
+		int n = s.nextInt();
+		int A[] = new int[n];
+		for (int i = 0; i < n; i++)
+			A[i] = s.nextInt();
+		int num = 1;
+		boolean result = true;
+		for (int i = 0; i < n; i++) {
+			if (A[i] >= num) {
+				while (A[i] >= num) {
+					stack.add(num);
+					num++;
+					sb.append("+\n");
+				}
+				stack.pop();
+				sb.append("-\n");
+			}
+			else {
+				if(stack.peek() == A[i]) {
+					stack.pop();
+					sb.append("-\n");
+				}
+				else {
+					System.out.println("NO");
+					result = false;
+					break;
+				}
+			}
 		}
-		bw.flush();
-		bw.close();
+		if(result)
+			System.out.println(sb);
 	}
 }
