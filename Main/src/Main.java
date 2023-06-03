8cc989aef1a6ef35b3e4c4689809d1ae18aa8955
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-03 17:00:19 +0900

    1427번 - 소트인사이드

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,43 +1,15 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
-	static class Node implements Comparable<Node>{
-		public int index;
-		public int value;
-		
-		public Node(int index, int value) {
-			this.index = index;
-			this.value = value;
-		}
-		
-		public int compareTo(Node o) {
-			return this.value - o.value;
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
-		Node A[] = new Node[N];
-		for (int i = 0; i < N; i++) 
-			A[i] = new Node(i, Integer.parseInt(bf.readLine()));
+		Scanner s = new Scanner(System.in);
+		char A[] = s.next().toCharArray();
 		Arrays.sort(A);
-		int answer = 0;
-		for(int i=0;i<N;i++) 
-			if(answer <A[i].index - i)
-				answer = A[i].index - i;
-		System.out.print(answer + 1);
-		
+		for (int i = A.length - 1; i >= 0; i--)
+			System.out.print(A[i] - '0');
+
 	}
 }
