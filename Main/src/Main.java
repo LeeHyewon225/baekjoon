110679ef1447671c04ff348d6dd04ad340683c0f
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-03 15:16:47 +0900

    2750번 - 수 정렬하기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,29 +1,45 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
+	static class Node {
+		public int index;
+		public long value;
+
+		public Node(int a, long b) {
+			index = a;
+			value = b;
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
-		PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> {
-			if (Math.abs(o1) == Math.abs(o2))
-				return o1 < o2 ? -1 : 1;
-			else
-				return Math.abs(o1) - Math.abs(o2);
-		});
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		int A[] = new int[N];
 		for (int i = 0; i < N; i++) {
-			int a = Integer.parseInt(bf.readLine());
-			if (a != 0)
-				queue.add(a);
-			else {
-				System.out.println(!queue.isEmpty() ? queue.poll() : 0);
+			A[i] = s.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N - i - 1; j++) {
+				if (A[j] > A[j + 1]) {
+					int temp = A[j];
+					A[j] = A[j + 1];
+					A[j + 1] = temp;
+				}
 			}
+		}
+		for (int i = 0; i < N; i++) {
+			System.out.println(A[i]);
 		}
 	}
 }
