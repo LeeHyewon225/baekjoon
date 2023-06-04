0347e7b240aa874827eb1b0ddd40c011c967efa8
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-05 01:47:28 +0900

    11004번 - K번째 수

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -9,39 +9,57 @@ public class Main {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		int A[] = new int[N];
+		st = new StringTokenizer(bf.readLine());
 		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(bf.readLine());
+			A[i] = Integer.parseInt(st.nextToken());
 		}
-		pivot(A, 0, N - 1);
-		for (int i = 0; i < N; i++)
-			System.out.println(A[i] + " ");
+		pivot(A, 0, N - 1, K);
+		System.out.println(A[K - 1]);
 	}
 
-	static void pivot(int A[], int s, int e) {
+	static void pivot(int A[], int s, int e, int K) {
+		if (s >= e)
+			return;
 		int start = s;
 		int end = e;
-		int p = e;
-		if (s > e)
-			return;
-		while (s != e) {
-			if (A[s] < A[p])
-				s++;
-			else if (A[e] >= A[p])
-				e--;
-			else if (A[s] > A[p] && A[e] < A[p]) {
+		int p;
+		if (s + 1 == e) {
+			if (A[s] > A[e]) {
 				int temp = A[s];
 				A[s] = A[e];
 				A[e] = temp;
-				s++;
 			}
+			p = e;
+		} else {
+			int m = (s + e) / 2;
+			int temp = A[s];
+			A[s] = A[m];
+			A[m] = temp;
+			s++;
+			while (s <= e) {
+				while (A[e] > A[start] && e > 0)
+					e--;
+				while (A[s] < A[start] && s < A.length - 1)
+					s++; 
+				if (s <= e) {
+					temp = A[s];
+					A[s] = A[e];
+					A[e] = temp;
+					s++;
+					e--;
+				}
+			}
+			temp = A[start];
+			A[start] = A[e];
+			A[e] = temp;
+			p = e;
 		}
-		
-		int temp = A[s];
-		A[s] = A[p];
-		A[p] = temp;
-		
-		pivot(A, start, s - 1);
-		pivot(A, s + 1, end);
+		if (K - 1 < p)
+			pivot(A, start, p - 1, K);
+		else if (p < K - 1)
+			pivot(A, p + 1, end, K);
+
 	}
 }
