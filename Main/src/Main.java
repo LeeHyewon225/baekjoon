d76f21fe694259d4adcb6d3b7868f6db15bac670
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-08 21:54:25 +0900

    1167번 - 트리의 지름

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,54 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int x[] = { -1, 1, 0, 0 };
-	static int y[] = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		char A[][] = new char[N][M];
+		StringTokenizer st;
+		int N = Integer.parseInt(bf.readLine());
+		ArrayList<Node> A[] = new ArrayList[N];
+		for (int i = 0; i < N; i++)
+			A[i] = new ArrayList<Node>();
 		for (int i = 0; i < N; i++) {
-			A[i] = bf.readLine().toCharArray();
-			for (int j = 0; j < N; j++) {
-
+			st = new StringTokenizer(bf.readLine());
+			int v1 = Integer.parseInt(st.nextToken()) - 1;
+			while (true) {
+				int v2 = Integer.parseInt(st.nextToken()) - 1;
+				if (v2 == -2)
+					break;
+				int d = Integer.parseInt(st.nextToken());
+				A[v1].add(new Node(v2, d));
 			}
 		}
-		int visit[][] = new int[N][M];
-		BFS(0, 0, A, visit);
+		int visit[] = new int[N];
+		long distance[] = new long[N];
+		BFS(0, A, visit, distance);
+		int max_index = 0;
+		for (int i = 0; i < N; i++)
+			if (distance[i] > distance[max_index])
+				max_index = i;
+		visit = new int[N];
+		distance = new long[N];
+		BFS(max_index, A, visit, distance);
+		Arrays.sort(distance);
+		System.out.println(distance[N - 1]);
 	}
 
-	static void BFS(int i, int j, char A[][], int visit[][]) {
-		Queue<int[]> queue = new LinkedList<int[]>();
-		int a[] = { i, j };
-		queue.add(a);
-		visit[i][j] = 1;
+	static void BFS(int v, ArrayList<Node> A[], int visit[], long distance[]) {
+		Queue<Integer> queue = new LinkedList<Integer>();
+		visit[v] = 1;
+		for (int i = 0; i < A[v].size(); i++) {
+			visit[A[v].get(i).V] = 1;
+			queue.add(A[v].get(i).V);
+			distance[A[v].get(i).V] = A[v].get(i).D;
+		}
 		while (!queue.isEmpty()) {
-			int now[] = queue.poll();
-			visit[now[0]][now[1]] = 1;
-			if (now[0] == N - 1 && now[1] == M - 1) {
-				System.out.println(A[now[0]][now[1]] - '0');
-				break;
-			}
-			for (int k = 0; k < 4; k++) {
-				int x1 = now[0] + x[k];
-				int y1 = now[1] + y[k];
-				if (x1 >= 0 && x1 < N && y1 >= 0 && y1 < M) {
-					if (A[x1][y1] != '0' && visit[x1][y1] == 0) {
-						visit[x1][y1] = 1;
-						A[x1][y1] = (char) (A[now[0]][now[1]] + 1);
-						queue.add(new int[] { x1, y1 });
-					}
+			int now = queue.poll();
+			for (int i = 0; i < A[now].size(); i++) {
+				Node next = A[now].get(i);
+				if (visit[next.V] == 0) {
+					visit[next.V] = 1;
+					queue.add(next.V);
+					distance[next.V] = distance[now] + next.D;
 				}
 			}
 		}
+	}
+}
+
+class Node {
+	int V;
+	int D;
+
+	public Node(int v, int d) {
+		V = v;
+		D = d;
 	}
 }
