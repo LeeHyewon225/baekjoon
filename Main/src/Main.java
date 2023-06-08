34a7100707c08f0b0bfc0faec1e5994c4741a6fc
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-08 20:33:54 +0900

    2178번 - 미로 탐색

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,61 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int N, M;
+	static int x[] = { -1, 1, 0, 0 };
+	static int y[] = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int V = Integer.parseInt(st.nextToken()) - 1;
-		ArrayList<Integer> A[] = new ArrayList[N];
-		for (int i = 0; i < N; i++)
-			A[i] = new ArrayList<Integer>();
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(bf.readLine());
-			int a = Integer.parseInt(st.nextToken()) - 1;
-			int b = Integer.parseInt(st.nextToken()) - 1;
-			A[a].add(b);
-			A[b].add(a);
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		char A[][] = new char[N][M];
+		for (int i = 0; i < N; i++) {
+			A[i] = bf.readLine().toCharArray();
+			for (int j = 0; j < N; j++) {
+
+			}
 		}
-		for (int i = 0; i < N; i++)
-			Collections.sort(A[i]);
-		int visit[] = new int[N];
-		DFS(V, A, visit);
-		System.out.println();
-		visit = new int[N];
-		BFS(V, A, visit);
+		int visit[][] = new int[N][M];
+		BFS(0, 0, A, visit);
 	}
 
-	static void DFS(int V, ArrayList<Integer>[] A, int visit[]) {
-		System.out.print(V + 1 + " ");
-		visit[V] = 1;
-		for (int j = 0; j < A[V].size(); j++) {
-			if (visit[(int) A[V].get(j)] == 0)
-				DFS((int) A[V].get(j), A, visit);
-		}
-
-	}
-
-	static void BFS(int V, ArrayList<Integer>[] A, int visit[]) {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		queue.add(V);
-		visit[V] = 1;
+	static void BFS(int i, int j, char A[][], int visit[][]) {
+		Queue<int[]> queue = new LinkedList<int[]>();
+		int a[] = { i, j };
+		queue.add(a);
+		visit[i][j] = 1;
 		while (!queue.isEmpty()) {
-			V = queue.poll();
-			System.out.print(V + 1 + " ");
-			for (int i = 0; i < A[V].size(); i++)
-				if (visit[A[V].get(i)] == 0) {
-					visit[A[V].get(i)] = 1;
-					queue.add(A[V].get(i));
+			int now[] = queue.poll();
+			visit[now[0]][now[1]] = 1;
+			if (now[0] == N - 1 && now[1] == M - 1) {
+				System.out.println(A[now[0]][now[1]] - '0');
+				break;
+			}
+			for (int k = 0; k < 4; k++) {
+				int x1 = now[0] + x[k];
+				int y1 = now[1] + y[k];
+				if (x1 >= 0 && x1 < N && y1 >= 0 && y1 < M) {
+					if (A[x1][y1] != '0' && visit[x1][y1] == 0) {
+						visit[x1][y1] = 1;
+						A[x1][y1] = (char) (A[now[0]][now[1]] + 1);
+						queue.add(new int[] { x1, y1 });
+					}
 				}
+			}
 		}
 	}
 }
