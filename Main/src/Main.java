fc0b38a0920defc553f49b7a37006e0b75e2f1b9
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-09 16:16:34 +0900

    11403번 - 경로 찾기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,31 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(bf.readLine());
-		int k = Integer.parseInt(bf.readLine());
-		int s = 1;
-		int e = k;
-		int answer = 0;
-		while (s <= e) {
-			int m = (s + e) / 2;
-			int sum = 0;
-			for (int i = 1; i <= N; i++) {
-				if (i > m)
-					break;
-				sum = sum + (m / i > N ? N : m / i);
-			}
-			if (sum < k) 
-				s = m + 1;
-			else if (sum >= k) {
-				e = m - 1;
-				answer = m;
+		StringTokenizer st;
+		ArrayList<Integer> A[] = new ArrayList[N];
+		int visit[];
+		for (int i = 0; i < N; i++) {
+			A[i] = new ArrayList<Integer>();
+			st = new StringTokenizer(bf.readLine());
+			for (int j = 0; j < N; j++) {
+				int a = Integer.parseInt(st.nextToken());
+				if (a == 1)
+					A[i].add(j);
 			}
 		}
-		System.out.println(answer);
+		for (int i = 0; i < N; i++) {
+			visit = new int[N];
+			DFS(i, A, visit);
+			for (int j = 0; j < N; j++)
+				System.out.print(visit[j] + " ");
+			System.out.println();
+		}
+	}
+
+	static void DFS(int num, ArrayList<Integer> A[], int visit[]) {
+		for (int i = 0; i < A[num].size(); i++) {
+			if (visit[A[num].get(i)] == 0) {
+				visit[A[num].get(i)] = 1;
+				DFS(A[num].get(i), A, visit);
+			}
+		}
 	}
 }
