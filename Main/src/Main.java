d73c011d5eff556a2bb43c2798d93c901fd4f709
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-09 17:27:18 +0900

    14940번 - 쉬운 최단거리

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -3,29 +3,73 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int dx[] = { -1, 1, 0, 0 };
+	static int dy[] = { 0, 0, -1, 1 };
+	static int N, M;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st= new StringTokenizer(bf.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		HashMap<String, String> a = new HashMap<String, String>();
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int map[][] = new int[N][M];
+		int distance[][] = new int[N][M];
+		int x = 0, y = 0;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(bf.readLine());
-			String site = st.nextToken();
-			String password = st.nextToken();
-			a.put(site, password);
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 2) {
+					x = i;
+					y = j;
+				}
+			}
 		}
-		for(int i=0;i<M;i++) {
-			String str = bf.readLine();
-			bw.write(a.get(str) + "\n");
+		BFS(x, y, map, distance);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++)
+				if (i == x && j == y)
+					bw.write("0 ");
+				else if (map[i][j] == 0)
+					bw.write(Integer.toString(distance[i][j]) + " ");
+				else
+					bw.write(Integer.toString(distance[i][j] - 1) + " ");
+			bw.write("\n");
 		}
 		bw.flush();
 		bw.close();
+	}
+
+	static void BFS(int x, int y, int map[][], int distance[][]) {
+		Queue<Node> q = new LinkedList();
+		q.add(new Node(x, y));
+		distance[x][y] = 1;
+		while (!q.isEmpty()) {
+			Node now = q.poll();
+			for (int i = 0; i < 4; i++) {
+				int x2 = now.x + dx[i];
+				int y2 = now.y + dy[i];
+				if (x2 >= 0 && x2 < N && y2 >= 0 && y2 < M && map[x2][y2] == 1 && distance[x2][y2] == 0) {
+					q.add(new Node(x2, y2));
+					distance[x2][y2] = distance[now.x][now.y] + 1;
+				}
+			}
+		}
+	}
+
+	static class Node {
+		int x;
+		int y;
+
+		public Node(int a, int b) {
+			x = a;
+			y = b;
+		}
 	}
 }
