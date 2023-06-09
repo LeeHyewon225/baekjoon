a9c09612d26aa27a500b16325d930cf10634eb42
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-09 14:39:23 +0900

    2343번 - 기타 레슨

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,38 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
 		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 		int A[] = new int[N];
-		for (int i = 0; i < N; i++)
-			A[i] = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(bf.readLine());
-		Arrays.sort(A);
 		st = new StringTokenizer(bf.readLine());
-		for (int i = 0; i < M; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			int s = 0;
-			int e = N - 1;
-			boolean result = false;
-			while (s <= e) {
-				int index = (s + e) / 2;
-				if (A[index] == num) {
-					result = true;
-					break;
-				} else if (A[index] < num) {
-					s = index + 1;
-				} else {
-					e = index - 1;
-				}
-			}
-			System.out.println(result ? 1 : 0);
+		int s = 0;
+		int e = 0;
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+			if (s < A[i])
+				s = A[i];
+			e += A[i];
 		}
+		int m = 0;
+		while (s <= e) {
+			int sum = 0;
+			int count = 0;
+			m = (s + e) / 2;
+			for (int i = 0; i < N; i++) {
+				sum += A[i];
+				if (sum > m) {
+					sum = A[i];
+					count++;
+				}
+				if (count > M)
+					break;
+			}
+			if (count >= M) {
+				s = m + 1;
+			}
+			else if(count < M) {
+				e = m-1;
+			}
+		}
+		System.out.println(s);
 	}
 }
