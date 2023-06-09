1db14bb2d3ec652413b2be8822506b5a44e3b816
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-09 15:33:47 +0900

    1300번 - K번째 수

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,46 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int A[] = new int[N];
-		st = new StringTokenizer(bf.readLine());
-		int s = 0;
-		int e = 0;
-		for (int i = 0; i < N; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-			if (s < A[i])
-				s = A[i];
-			e += A[i];
-		}
-		int m = 0;
+		int N = Integer.parseInt(bf.readLine());
+		int k = Integer.parseInt(bf.readLine());
+		int s = 1;
+		int e = k;
+		int answer = 0;
 		while (s <= e) {
+			int m = (s + e) / 2;
 			int sum = 0;
-			int count = 0;
-			m = (s + e) / 2;
-			for (int i = 0; i < N; i++) {
-				sum += A[i];
-				if (sum > m) {
-					sum = A[i];
-					count++;
-				}
-				if (count > M)
+			for (int i = 1; i <= N; i++) {
+				if (i > m)
 					break;
+				sum = sum + (m / i > N ? N : m / i);
 			}
-			if (count >= M) {
+			if (sum < k) 
 				s = m + 1;
-			}
-			else if(count < M) {
-				e = m-1;
+			else if (sum >= k) {
+				e = m - 1;
+				answer = m;
 			}
 		}
-		System.out.println(s);
+		System.out.println(answer);
 	}
 }
