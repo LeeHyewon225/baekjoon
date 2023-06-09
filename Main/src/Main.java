e2ad9b4ca8eadb723d1e0e419c32325bf97af333
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-09 14:07:44 +0900

    1920번 - 수 찾기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,74 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		int N = Integer.parseInt(bf.readLine());
-		ArrayList<Node> A[] = new ArrayList[N];
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		int A[] = new int[N];
 		for (int i = 0; i < N; i++)
-			A[i] = new ArrayList<Node>();
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(bf.readLine());
-			int v1 = Integer.parseInt(st.nextToken()) - 1;
-			while (true) {
-				int v2 = Integer.parseInt(st.nextToken()) - 1;
-				if (v2 == -2)
+			A[i] = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(bf.readLine());
+		Arrays.sort(A);
+		st = new StringTokenizer(bf.readLine());
+		for (int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			int s = 0;
+			int e = N - 1;
+			boolean result = false;
+			while (s <= e) {
+				int index = (s + e) / 2;
+				if (A[index] == num) {
+					result = true;
 					break;
-				int d = Integer.parseInt(st.nextToken());
-				A[v1].add(new Node(v2, d));
-			}
-		}
-		int visit[] = new int[N];
-		long distance[] = new long[N];
-		BFS(0, A, visit, distance);
-		int max_index = 0;
-		for (int i = 0; i < N; i++)
-			if (distance[i] > distance[max_index])
-				max_index = i;
-		visit = new int[N];
-		distance = new long[N];
-		BFS(max_index, A, visit, distance);
-		Arrays.sort(distance);
-		System.out.println(distance[N - 1]);
-	}
-
-	static void BFS(int v, ArrayList<Node> A[], int visit[], long distance[]) {
-		Queue<Integer> queue = new LinkedList<Integer>();
-		visit[v] = 1;
-		for (int i = 0; i < A[v].size(); i++) {
-			visit[A[v].get(i).V] = 1;
-			queue.add(A[v].get(i).V);
-			distance[A[v].get(i).V] = A[v].get(i).D;
-		}
-		while (!queue.isEmpty()) {
-			int now = queue.poll();
-			for (int i = 0; i < A[now].size(); i++) {
-				Node next = A[now].get(i);
-				if (visit[next.V] == 0) {
-					visit[next.V] = 1;
-					queue.add(next.V);
-					distance[next.V] = distance[now] + next.D;
+				} else if (A[index] < num) {
+					s = index + 1;
+				} else {
+					e = index - 1;
 				}
 			}
+			System.out.println(result ? 1 : 0);
 		}
-	}
-}
-
-class Node {
-	int V;
-	int D;
-
-	public Node(int v, int d) {
-		V = v;
-		D = d;
 	}
 }
