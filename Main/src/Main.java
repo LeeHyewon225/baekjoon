152506fa20238e3c9ee698675a59509a374b30d2
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-10 23:08:36 +0900

    25592번 - 바둑돌 게임

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,75 +1,26 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-	static int dx[] = { -1, 1, 0, 0 };
-	static int dy[] = { 0, 0, -1, 1 };
-	static int N, M;
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		int map[][] = new int[N][M];
-		int distance[][] = new int[N][M];
-		int x = 0, y = 0;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(bf.readLine());
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == 2) {
-					x = i;
-					y = j;
-				}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		int i = 1;
+		int sum = 0;
+		int answer;
+		boolean winner = false;
+		while(true) {
+			sum += i;
+			i++;
+			if(sum > N) {
+				answer = (winner) ? 0 : sum - N;
+				break;
 			}
-		}
-		BFS(x, y, map, distance);
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++)
-				if (i == x && j == y)
-					bw.write("0 ");
-				else if (map[i][j] == 0)
-					bw.write(Integer.toString(distance[i][j]) + " ");
-				else
-					bw.write(Integer.toString(distance[i][j] - 1) + " ");
-			bw.write("\n");
-		}
-		bw.flush();
-		bw.close();
-	}
-
-	static void BFS(int x, int y, int map[][], int distance[][]) {
-		Queue<Node> q = new LinkedList();
-		q.add(new Node(x, y));
-		distance[x][y] = 1;
-		while (!q.isEmpty()) {
-			Node now = q.poll();
-			for (int i = 0; i < 4; i++) {
-				int x2 = now.x + dx[i];
-				int y2 = now.y + dy[i];
-				if (x2 >= 0 && x2 < N && y2 >= 0 && y2 < M && map[x2][y2] == 1 && distance[x2][y2] == 0) {
-					q.add(new Node(x2, y2));
-					distance[x2][y2] = distance[now.x][now.y] + 1;
-				}
+			else if(sum == N) {
+				answer = (winner) ? sum + i  - N: 0;
+				break;
 			}
+			winner = !winner;
 		}
-	}
-
-	static class Node {
-		int x;
-		int y;
-
-		public Node(int a, int b) {
-			x = a;
-			y = b;
-		}
+		System.out.print(answer);
 	}
 }
