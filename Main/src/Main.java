0bebd741cbfd1e5009afb9d34fca8f782408937b
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-10 23:10:33 +0900

    25591번 - 푸앙이와 종윤이

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,26 +1,19 @@
 import java.util.Scanner;
-
 public class Main {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		int i = 1;
-		int sum = 0;
-		int answer;
-		boolean winner = false;
-		while(true) {
-			sum += i;
-			i++;
-			if(sum > N) {
-				answer = (winner) ? 0 : sum - N;
-				break;
-			}
-			else if(sum == N) {
-				answer = (winner) ? sum + i  - N: 0;
-				break;
-			}
-			winner = !winner;
-		}
-		System.out.print(answer);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+		int a = 100 - x;
+		int b = 100 - y;
+		int c = 100 - a - b;
+		int d = a * b;
+		int q = d / 100;
+		int r = d % 100;
+		System.out.println(a + " " + b + " " + c + " " + d + " " + q + " " + r);
+		System.out.println((c+q) + " " + r);
 	}
+
 }
