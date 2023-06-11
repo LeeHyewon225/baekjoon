fc09c95d4f875e9f824a6581e0707cd066d3e8c4
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-11 17:15:28 +0900

    1541번 - 잃어버린 괄호

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,38 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int a[][] = new int[N][2];
-		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			a[i][0] = Integer.parseInt(st.nextToken());
-			a[i][1] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(a, new Comparator<int[]>() {
-			public int compare(int[] o1, int[] o2) {
-				if (o1[1] != o2[1])
-					return o1[1] - o2[1];
-				else
-					return o1[0] - o2[0];
+		StringTokenizer st1 = new StringTokenizer(br.readLine(), "-");
+		boolean first = true;
+		int result = 0;
+		while (st1.countTokens() > 0) {
+			StringTokenizer st2 = new StringTokenizer(st1.nextToken(), "+");
+			int sum = 0;
+			while (st2.countTokens() > 0) {
+				int num = Integer.parseInt(st2.nextToken());
+				sum += num;
 			}
-		});
-		int count = 1;
-		int time = a[0][1];
-		for (int i = 1; i < N; i++) {
-			if (a[i][0] >= time) {
-				time = a[i][1];
-				count++;
-			}
+			result += first ? sum : -sum;
+			first = false;
 		}
-		System.out.println(count);
+		System.out.println(result);
 	}
 }
