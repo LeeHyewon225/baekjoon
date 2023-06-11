81daaba1eca1e5e107e13f8124231c20f33ed03c
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-11 19:22:07 +0900

    16953번 - A → B

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,43 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		ArrayList<Integer> a[] = new ArrayList[N];
-		int count =0;
-		for(int i=0;i<N;i++) {		
-			a[i] = new ArrayList<Integer>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int count = 0;
+		while (A < B) {
+			if (B % 10 == 1)
+				B /= 10;
+			else if(B % 2 == 1)
+				break;
+			else
+				B /= 2;
+			count++;
 		}
-		for(int i=0;i<N-1;i++) {		
-			st = new StringTokenizer(br.readLine());
-			int n1 = Integer.parseInt(st.nextToken()) - 1;
-			int n2 = Integer.parseInt(st.nextToken()) - 1;
-			a[n1].add(n2);
-			a[n2].add(n1);
-		}
-		int parent[] = new int[N];
-		int visit[] = new int[N];
-		visit[0] = 1;
-		DFS(0, a, parent, visit);
-		for(int i=1;i<N;i++) 
-			System.out.println(parent[i] + 1);
-		
-	}
-	static void DFS(int n, ArrayList a[], int parent[], int visit[]) {
-		for(int i=0;i<a[n].size();i++) {
-			int temp = (int) a[n].get(i);
-			if(visit[temp]==0) {
-				visit[temp]=1;
-				parent[temp] = n;
-				DFS(temp, a, parent, visit);
-			}
-		}
+		System.out.println(A == B ? count + 1 : -1);
 	}
 }
