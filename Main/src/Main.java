e3c8aa018407784f943c2dea61684c951311527e
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-11 16:55:02 +0900

    1931번 - 회의실 배정

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,39 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> queue1 = new PriorityQueue<>();
-		PriorityQueue<Integer> queue2 = new PriorityQueue<>(Collections.reverseOrder());
-		boolean zero = false;
-		int one_count = 0;
+		int a[][] = new int[N][2];
+		StringTokenizer st;
 		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(br.readLine());
-			if (num < 0)
-				queue1.add(num);
-			else if (num > 1)
-				queue2.add(num);
-			else if (num == 0)
-				zero = true;
-			else
-				one_count++;
-
+			st = new StringTokenizer(br.readLine());
+			a[i][0] = Integer.parseInt(st.nextToken());
+			a[i][1] = Integer.parseInt(st.nextToken());
 		}
-		int result = 0;
-		while (queue1.size() > 1)
-			result += queue1.remove() * queue1.remove();
-		while (queue2.size() > 1)
-			result += queue2.remove() * queue2.remove();
-		if (queue2.size() == 1)
-			result += queue2.remove();
-		if (queue1.size() == 1 && !zero)
-			result += queue1.remove();
-		System.out.println(result + one_count);
+		Arrays.sort(a, new Comparator<int[]>() {
+			public int compare(int[] o1, int[] o2) {
+				if (o1[1] != o2[1])
+					return o1[1] - o2[1];
+				else
+					return o1[0] - o2[0];
+			}
+		});
+		int count = 1;
+		int time = a[0][1];
+		for (int i = 1; i < N; i++) {
+			if (a[i][0] >= time) {
+				time = a[i][1];
+				count++;
+			}
+		}
+		System.out.println(count);
 	}
 }
