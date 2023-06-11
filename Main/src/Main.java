420b80d42b61b2bbd7f67c46458a688ef10f1dee
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-11 16:21:41 +0900

    1744번 - 수 묶기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Main {
@@ -8,16 +9,31 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> a = new PriorityQueue<Integer>();
-		for (int i = 0; i < N; i++)
-			a.add(Integer.parseInt(br.readLine()));
-		int result = 0;
-		while (a.size() > 1) {
-			int sum = a.remove() + a.remove();
-			a.add(sum);
-			result += sum;
-		}
-		System.out.println(result);
+		PriorityQueue<Integer> queue1 = new PriorityQueue<>();
+		PriorityQueue<Integer> queue2 = new PriorityQueue<>(Collections.reverseOrder());
+		boolean zero = false;
+		int one_count = 0;
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num < 0)
+				queue1.add(num);
+			else if (num > 1)
+				queue2.add(num);
+			else if (num == 0)
+				zero = true;
+			else
+				one_count++;
 
+		}
+		int result = 0;
+		while (queue1.size() > 1)
+			result += queue1.remove() * queue1.remove();
+		while (queue2.size() > 1)
+			result += queue2.remove() * queue2.remove();
+		if (queue2.size() == 1)
+			result += queue2.remove();
+		if (queue1.size() == 1 && !zero)
+			result += queue1.remove();
+		System.out.println(result + one_count);
 	}
 }
