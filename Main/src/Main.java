c87ea5cbb76aa4b7bf8702cd7248032e9877240a
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-11 15:42:01 +0900

    1715번 - 카드 정렬하기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,25 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.PriorityQueue;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int money[] = new int[N];
-		for (int i = 0; i < N; i++) 
-			money[i] = Integer.parseInt(br.readLine());
-		int count = 0;
-		for (int i = N - 1; i >= 0; i--) {
-			if (money[i] <= K) {
-				count += K / money[i];
-				K = K % money[i];
-			}
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> a = new PriorityQueue<Integer>();
+		for (int i = 0; i < N; i++)
+			a.add(Integer.parseInt(br.readLine()));
+		int result = 0;
+		while (a.size() > 1) {
+			int sum = a.remove() + a.remove();
+			a.add(sum);
+			result += sum;
 		}
-		System.out.println(count);
+		System.out.println(result);
+
 	}
 }
