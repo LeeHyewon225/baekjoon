b1168d0dabe51311d530158ec6ed7557faf0b692
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-11 14:53:24 +0900

    11047번 - 동전 0

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,18 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		int x = scanner.nextInt();
-		int y = scanner.nextInt();
-		int a = 100 - x;
-		int b = 100 - y;
-		int c = 100 - a - b;
-		int d = a * b;
-		int q = d / 100;
-		int r = d % 100;
-		System.out.println(a + " " + b + " " + c + " " + d + " " + q + " " + r);
-		System.out.println((c+q) + " " + r);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int money[] = new int[N];
+		int j = 0;
+		for (int i = 0; i < N; i++) {
+			money[i] = Integer.parseInt(br.readLine());
+			if (money[i] <= K)
+				j = i;
+		}
+		int now = 0;
+		int count = 0;
+		while (K > 0) {
+			int c = K / money[j];
+			now += c * money[j];
+			K -= c * money[j];
+			j--;
+			count += c;
+		}
+		System.out.println(count);
 	}
 }
