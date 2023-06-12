303b0ab5ce7a1b4d359603e558084dc80b2ea1a8
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-12 19:06:07 +0900

    1016번 - 제곱 ㄴㄴ 수

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,26 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		boolean prime[] = new boolean[10000001];
-		prime[1] = true;
-		for (int i = 2; i <= Math.sqrt(10000000); i++) {
-			if (!prime[i])
-				for (int j = 2; j * i <= 10000000; j++) {
-					prime[i * j] = true;
-				}
-		}
-		for (int i = N; i <= 10000000; i++)
-			if (!prime[i]) {
-				if(i==Integer.parseInt(new StringBuffer(String.valueOf(i)).reverse().toString())) {
-					System.out.println(i);
-					break;
-				}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long min = Long.parseLong(st.nextToken());
+		long max = Long.parseLong(st.nextToken());
+		boolean a[] = new boolean[1000001];
+		for (long i = 2; i <= Math.sqrt(max); i++) {
+			long n = i * i;
+			long start =  ((min + n - 1) / n);
+			for (long j = start; j * n <= max; j++) {
+				a[(int) (j * n - min)] = true;  
 			}
+		}
+		int count = 0;
+		for (int i = 0; i <= max - min; i++)
+			if (!a[i]) 
+				count++;
+		System.out.println(count);
 	}
 }
