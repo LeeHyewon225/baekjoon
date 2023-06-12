dc3a3c06d75693be295cdda8f2cc0c6a63367a6f
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-12 17:33:14 +0900

    1456번 - 거의 소수

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -8,18 +8,31 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		boolean a[] = new boolean[N + 1];
-		a[1] = true;
-		for (int i = 2; i <= N; i++) {
-			if (!a[i])
-				for (int j = 2; j * i <= N; j++) {
-					a[i * j] = true;
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		boolean prime[] = new boolean[10000001];
+		prime[1] = true;
+		for (int i = 2; i <= Math.sqrt(10000000); i++) {
+			if (!prime[i])
+				for (int j = 2; j * i <= 10000000; j++) {
+					prime[i * j] = true;
 				}
 		}
-		for (int i = M; i <= N; i++)
-			if (!a[i])
-				System.out.println(i);
+		int count=0;
+		for (int i = 2; i <= 10000000; i++)
+			if (!prime[i]) {
+				double k = i;
+				double a = A / k;
+				double b = B / k;
+				while(true) {
+					if(k > b)
+						break;
+					if(k >= a)
+						count++;
+					a /= k;
+					b /= k;
+				}
+			}
+		System.out.println(count);
 	}
 }
