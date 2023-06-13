2868f5407ce13fc41764254a0b412d290d0794df
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-13 16:28:25 +0900

    11689번 - GCD(N, K) = 1

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,27 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long min = Long.parseLong(st.nextToken());
-		long max = Long.parseLong(st.nextToken());
-		boolean a[] = new boolean[1000001];
-		for (long i = 2; i <= Math.sqrt(max); i++) {
-			long n = i * i;
-			long start =  ((min + n - 1) / n);
-			for (long j = start; j * n <= max; j++) {
-				a[(int) (j * n - min)] = true;  
+		long n = Long.parseLong(br.readLine());
+		long result = n;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (n % i == 0) {
+				result -= result / i;
+				while (n % i == 0)
+					n /= i;
 			}
 		}
-		int count = 0;
-		for (int i = 0; i <= max - min; i++)
-			if (!a[i]) 
-				count++;
-		System.out.println(count);
+		System.out.println(n > 1 ? result - result / n : result);
 	}
 }
