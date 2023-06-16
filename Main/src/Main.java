5536ea587da40b2dec2384a2179d793b0e8a4e26
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-16 23:22:02 +0900

    2573번 - 빙산

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,66 +1,90 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int dx[] = { -1, 1, 0, 0 };
+	static int dy[] = { 0, 0, -1, 1 };
+	static int map[][];
+	static int visit[][];
+	static int N;
+	static int M;
+	static int count = 0;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		ArrayList<Node> a[] = new ArrayList[N];
-		int visit[] = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
 		for (int i = 0; i < N; i++) {
-			a[i] = new ArrayList<Node>();
-			visit[i] = -1;
-		}
-		for (int j = 0; j < N - 1; j++) {
 			st = new StringTokenizer(br.readLine());
-			int n1 = Integer.parseInt(st.nextToken()) - 1;
-			int n2 = Integer.parseInt(st.nextToken()) - 1;
-			int v = Integer.parseInt(st.nextToken());
-			a[n1].add(new Node(n2, v));
-			a[n2].add(new Node(n1, v));
+			for (int j = 0; j < M; j++)
+				map[i][j] = Integer.parseInt(st.nextToken());
 		}
-		visit[0] = 0;
-		DFS(0, a, visit);
-		int max = 0;
-		int max_index = 0;
-		for (int i = 0; i < N; i++)
-			if (max < visit[i]) {
-				max = visit[i];
-				max_index = i;
-			}
-		visit = new int[N];
-		for (int i = 0; i < N; i++)
-			visit[i] = -1;
-		visit[max_index] = 0;
-		max = 0;
-		DFS(max_index, a, visit);
-		for (int i = 0; i < N; i++)
-			if (max < visit[i])
-				max = visit[i];
-		System.out.println(max);
-	}
-
-	static void DFS(int n, ArrayList<Node> a[], int visit[]) {
-		for (int i = 0; i < a[n].size(); i++) {
-			Node n2 = a[n].get(i);
-			if (visit[n2.node] == -1) {
-				visit[n2.node] = n2.value + visit[n];
-				DFS((int) n2.node, a, visit);
+		while (true) {
+			if (check_iceberg()) {
+				System.out.println(count);
+				break;
+			} else {
+				count++;
+				if (after_1year()) {
+					System.out.println("0");
+					break;
+				}
 			}
 		}
 	}
 
-	static class Node {
-		int node;
-		int value;
+	static boolean check_iceberg() {
+		visit = new int[N][M];
+		int count = 0;
+		for (int i = 0; i < N; i++) 
+			for (int j = 0; j < M; j++) 
+				if (map[i][j] > 0 && visit[i][j] == 0) {
+					if (count == 1)
+						return true;
+					count++;
+					DFS(i, j);
+				}
+		return false;
+	}
 
-		public Node(int node, int value) {
-			this.node = node;
-			this.value = value;
+	static void DFS(int x, int y) {
+		for (int i = 0; i < 4; i++) {
+			int x2 = x + dx[i];
+			int y2 = y + dy[i];
+			if (0 <= x2 && x2 < N && 0 <= y2 && y2 < M && visit[x2][y2] == 0) 
+				if (map[x2][y2] > 0) {
+					visit[x2][y2] = 1;
+					DFS(x2, y2);
+				}
 		}
+	}
+
+	static boolean after_1year() {
+		boolean melt_all = true;
+		int count_melt[][] = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (map[i][j] > 0) 
+					for (int k = 0; k < 4; k++) {
+						int x1 = i + dx[k];
+						int y1 = j + dy[k];
+						if (0 <= x1 && x1 < N && 0 <= y1 && y1 < M)
+							if (map[x1][y1] <= 0)
+								count_melt[i][j]++;
+					}
+				if (map[i][j] > 0)
+					melt_all = false;
+			}
+		}
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				if (map[i][j] > 0)
+					map[i][j] -= count_melt[i][j];
+
+		return melt_all;
 	}
 }
