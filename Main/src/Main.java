d518f507cbd5e996dd029da307d69aeb1a16b60b
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-17 01:48:38 +0900

    2206번 - 벽 부수고 이동하기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,90 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int dx[] = { -1, 1, 0, 0 };
 	static int dy[] = { 0, 0, -1, 1 };
-	static int map[][];
-	static int visit[][];
+	static char map[][];
+	static int visit[][][];
 	static int N;
 	static int M;
-	static int count = 0;
+	static int min = Integer.MAX_VALUE;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++)
-				map[i][j] = Integer.parseInt(st.nextToken());
-		}
-		while (true) {
-			if (check_iceberg()) {
-				System.out.println(count);
-				break;
-			} else {
-				count++;
-				if (after_1year()) {
-					System.out.println("0");
-					break;
-				}
-			}
-		}
-	}
-
-	static boolean check_iceberg() {
-		visit = new int[N][M];
-		int count = 0;
-		for (int i = 0; i < N; i++) 
-			for (int j = 0; j < M; j++) 
-				if (map[i][j] > 0 && visit[i][j] == 0) {
-					if (count == 1)
-						return true;
-					count++;
-					DFS(i, j);
-				}
-		return false;
+		map = new char[N][M];
+		visit = new int[N][M][2];
+		for (int i = 0; i < N; i++)
+			map[i] = br.readLine().toCharArray();
+		DFS(0, 0);
+		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
 	}
 
 	static void DFS(int x, int y) {
-		for (int i = 0; i < 4; i++) {
-			int x2 = x + dx[i];
-			int y2 = y + dy[i];
-			if (0 <= x2 && x2 < N && 0 <= y2 && y2 < M && visit[x2][y2] == 0) 
-				if (map[x2][y2] > 0) {
-					visit[x2][y2] = 1;
-					DFS(x2, y2);
+		Queue<Map> queue = new LinkedList<Map>();
+		visit[0][0][0] = 1;
+		queue.add(new Map(x, y, 0));
+		while (!queue.isEmpty()) {
+			Map now = queue.poll();
+			if (now.x == N - 1 && now.y == M - 1) {
+				min = visit[now.x][now.y][now.wall];
+				return;
+			} else
+				for (int i = 0; i < 4; i++) {
+					int x2 = now.x + dx[i];
+					int y2 = now.y + dy[i];
+					if (0 <= x2 && x2 < N && 0 <= y2 && y2 < M)
+						if (now.wall == 0) {
+							if (map[x2][y2] == '0' && visit[x2][y2][0] == 0) {
+								visit[x2][y2][0] = visit[now.x][now.y][0] + 1;
+								queue.add(new Map(x2, y2, now.wall));
+							} else if (now.wall == 0 && map[x2][y2] == '1') {
+								visit[x2][y2][1] = visit[now.x][now.y][0] + 1;
+								queue.add(new Map(x2, y2, 1));
+							}
+						} else if (map[x2][y2] == '0' && visit[x2][y2][1] == 0) {
+							visit[x2][y2][1] = visit[now.x][now.y][1] + 1;
+							queue.add(new Map(x2, y2, 1));
+						}
 				}
+
 		}
 	}
 
-	static boolean after_1year() {
-		boolean melt_all = true;
-		int count_melt[][] = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] > 0) 
-					for (int k = 0; k < 4; k++) {
-						int x1 = i + dx[k];
-						int y1 = j + dy[k];
-						if (0 <= x1 && x1 < N && 0 <= y1 && y1 < M)
-							if (map[x1][y1] <= 0)
-								count_melt[i][j]++;
-					}
-				if (map[i][j] > 0)
-					melt_all = false;
-			}
-		}
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < M; j++)
-				if (map[i][j] > 0)
-					map[i][j] -= count_melt[i][j];
+	static class Map {
+		int x;
+		int y;
+		int wall;
 
-		return melt_all;
+		public Map(int x, int y, int wall) {
+			this.x = x;
+			this.y = y;
+			this.wall = wall;
+		}
 	}
 }
