b930b0341eb839fd306927dd212c0202da939c92
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-17 23:24:24 +0900

    17144번 - 미세먼지 안녕!

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,68 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int dx[] = { -1, 1, 0, 0 };
+	static int dy[] = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int visit[] = new int[N];
-		st = new StringTokenizer(br.readLine());
-		int truth_count = Integer.parseInt(st.nextToken());
-		int truth[] = new int[N];
-		for (int i = 0; i < truth_count; i++)
-			truth[Integer.parseInt(st.nextToken()) - 1]++;
-		ArrayList<Integer> a[] = new ArrayList[N];
-		for (int i = 0; i < N; i++)
-			a[i] = new ArrayList<Integer>();
-		ArrayList<Integer> party[] = new ArrayList[M];
-		for (int i = 0; i < M; i++)
-			party[i] = new ArrayList<Integer>();
-		for (int i = 0; i < M; i++) {
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
+		int map[][] = new int[R][C];
+		int air_purifier = 0;
+		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
-			int party_count = Integer.parseInt(st.nextToken());
-			int person1 = Integer.parseInt(st.nextToken()) - 1;
-			party[i].add(person1);
-			for (int j = 1; j < party_count; j++) {
-				int person2 = Integer.parseInt(st.nextToken()) - 1;
-				party[i].add(person2);
-				a[person1].add(person2);
-				a[person2].add(person1);
-				person1 = person2;
+			for (int j = 0; j < C; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == -1)
+					air_purifier = i;
 			}
 		}
-		for (int i = 0; i < N; i++) {
-			if (truth[i] == 1 && visit[i] == 0) {
-				visit[i] = 1;
-				BFS(i, a, visit);
-			}
+		for (int i = 0; i < T; i++) {
+			spread_dust(R, C, map);
+			operate_air_purifier(R, C, map, air_purifier);
 		}
-		int count = 0;
-		for (int i = 0; i < M; i++) {
-			int lie = 1;
-			for (int j = 0; j < party[i].size(); j++) {
-				if (visit[party[i].get(j)] == 1) {
-					lie = 0;
-					break;
-				}
-			}
-			count += lie;
-		}
-		System.out.println(count);
+		int sum = 0;
+		for (int i = 0; i < R; i++)
+			for (int j = 0; j < C; j++)
+				sum += map[i][j];
+		System.out.println(sum + 2);
+
 	}
 
-	static void BFS(int n, ArrayList<Integer> a[], int visit[]) {
-		for (int i = 0; i < a[n].size(); i++) {
-			int now = (int) a[n].get(i);
-			if (visit[now] == 0) {
-				visit[now] = 1;
-				BFS(now, a, visit);
+	static void spread_dust(int R, int C, int map[][]) {
+		int map2[][] = new int[R][C];
+		for (int i = 0; i < R; i++)
+			for (int j = 0; j < C; j++) {
+				if (map[i][j] == -1)
+					continue;
+				if (map[i][j] != 0) {
+					int sum = 0;
+					for (int k = 0; k < 4; k++) {
+						int x = i + dx[k];
+						int y = j + dy[k];
+						if (0 <= x && x < R && 0 <= y && y < C && map[x][y] != -1) {
+							map2[x][y] += map[i][j] / 5;
+							sum += map[i][j] / 5;
+						}
+					}
+					map2[i][j] -= sum;
+				}
 			}
+		for (int i = 0; i < R; i++)
+			for (int j = 0; j < C; j++)
+				map[i][j] += map2[i][j];
+	}
+
+	static void operate_air_purifier(int R, int C, int map[][], int air_purifier) {
+		for (int i = air_purifier - 2; i > 0; i--)
+			map[i][0] = map[i - 1][0];
+		for (int i = air_purifier + 1; i < R - 1; i++)
+			map[i][0] = map[i + 1][0];
+		for (int i = 0; i < C - 1; i++) {
+			map[0][i] = map[0][i + 1];
+			map[R - 1][i] = map[R - 1][i + 1];
 		}
+		for (int i = R - 1; i > air_purifier; i--)
+			map[i][C - 1] = map[i - 1][C - 1];
+		for (int i = 0; i < air_purifier - 1; i++)
+			map[i][C - 1] = map[i + 1][C - 1];
+		for (int i = C - 1; i > 1; i--) {
+			map[air_purifier - 1][i] = map[air_purifier - 1][i - 1];
+			map[air_purifier][i] = map[air_purifier][i - 1];
+		}
+		map[air_purifier - 1][1] = 0;
+		map[air_purifier][1] = 0;
 	}
 }
