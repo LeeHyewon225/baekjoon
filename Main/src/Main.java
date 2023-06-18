48f0822551624d06c90d38309763c96002217d3f
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-19 01:41:34 +0900

    14938번 - 서강그라운드

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,51 +1,76 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int dx[] = { -1, 1, 0, 0 };
 	static int dy[] = { 0, 0, -1, 1 };
 	static int count = 0;
-	static int N;
+	static int max = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		int pipe[][] = new int[N][N];
-		int visit[][] = new int[N][N];
-		StringTokenizer st;
-		for (int i = 0; i < N; i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int item[] = new int[N];
+		for (int i = 0; i < N; i++)
+			item[i] = Integer.parseInt(st.nextToken());
+		ArrayList<Node> A[] = new ArrayList[N];
+		for (int i = 0; i < N; i++)
+			A[i] = new ArrayList<Node>();
+		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++)
-				pipe[i][j] = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			int t = Integer.parseInt(st.nextToken());
+			A[a].add(new Node(b, t));
+			A[b].add(new Node(a, t));
 		}
-		DFS(0, 1, 0, pipe, visit);
-		System.out.println(count);
+		for (int i = 0; i < N; i++) 
+			BFS(i, N, M, A, item);
+		System.out.println(max);
 	}
 
-	static void DFS(int i, int j, int state, int pipe[][], int visit[][]) {
-		if (i == N - 1 && j == N - 1) {
-			count++;
-			return;
+	static void BFS(int n, int N, int M, ArrayList<Node> A[], int item[]) {
+		PriorityQueue<Node> q = new PriorityQueue<Node>();
+		int visit[] = new int[N];
+		visit[n] = 1;
+		int item_count = item[n];
+		for (int i = 0; i < A[n].size(); i++) 
+			q.add(new Node(A[n].get(i).n, A[n].get(i).d));
+		while (!q.isEmpty()) {
+			Node now = q.poll();
+			if (visit[now.n] == 0 && now.d <= M) {
+				visit[now.n] = now.d;
+				item_count += item[now.n];
+				for (int i = 0; i < A[now.n].size(); i++) {
+					Node a = A[now.n].get(i);
+					q.add(new Node(a.n, now.d + a.d));
+				}
+			}
 		}
-		if (state != 1)
-			if (j + 1 < N && pipe[i][j + 1] == 0 && visit[i][j + 1] == 0) {
-				visit[i][j + 1] = 1;
-				DFS(i, j + 1, 0, pipe, visit);
-				visit[i][j + 1] = 0;
-			}
-		if (state != 0)
-			if (i + 1 < N && pipe[i + 1][j] == 0 && visit[i + 1][j] == 0) {
-				visit[i + 1][j] = 1;
-				DFS(i + 1, j, 1, pipe, visit);
-				visit[i + 1][j] = 0;
-			}
-		if (i + 1 < N && j + 1 < N && pipe[i + 1][j + 1] == 0 && pipe[i][j + 1] == 0 && pipe[i + 1][j] == 0
-				&& visit[i + 1][j + 1] == 0) {
-			visit[i + 1][j + 1] = 1;
-			DFS(i + 1, j + 1, 2, pipe, visit);
-			visit[i + 1][j + 1] = 0;
+		max = Math.max(max, item_count);
+	}
+
+	static class Node implements Comparable<Node>{
+		int n;
+		int d;
+
+		public Node(int n, int d) {
+			this.n = n;
+			this.d = d;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			// TODO Auto-generated method stub
+			return this.d - o.d;
 		}
 	}
 }
