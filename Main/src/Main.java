5a8080cf1f45d5dd115e35258df18b521d5c136b
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-18 23:37:58 +0900

    2638번 - 치즈

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -10,73 +10,65 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int R = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-		int T = Integer.parseInt(st.nextToken());
-		int map[][] = new int[R][C];
-		int air_purifier = 0;
-		for (int i = 0; i < R; i++) {
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int cheese[][] = new int[N][M];
+		int outside_air[][];
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < C; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-				if (map[i][j] == -1)
-					air_purifier = i;
-			}
+			for (int j = 0; j < M; j++)
+				cheese[i][j] = Integer.parseInt(st.nextToken());
 		}
-		for (int i = 0; i < T; i++) {
-			spread_dust(R, C, map);
-			operate_air_purifier(R, C, map, air_purifier);
+		int time = 0;
+		while (true) {
+			outside_air = new int[N][M];
+			outside_air[0][0] = 1;
+			visit_outside(0, 0, N, M, cheese, outside_air);
+			melt_cheese(N, M, cheese, outside_air);
+			time++;
+			if (check(cheese, N, M))
+				break;
 		}
-		int sum = 0;
-		for (int i = 0; i < R; i++)
-			for (int j = 0; j < C; j++)
-				sum += map[i][j];
-		System.out.println(sum + 2);
-
+		System.out.println(time);
 	}
 
-	static void spread_dust(int R, int C, int map[][]) {
-		int map2[][] = new int[R][C];
-		for (int i = 0; i < R; i++)
-			for (int j = 0; j < C; j++) {
-				if (map[i][j] == -1)
-					continue;
-				if (map[i][j] != 0) {
-					int sum = 0;
+	static void visit_outside(int r, int c, int N, int M, int cheese[][], int outside_air[][]) {
+		for (int i = 0; i < 4; i++) {
+			int x = r + dx[i];
+			int y = c + dy[i];
+			if (0 <= x && x < N && 0 <= y && y < M)
+				if (cheese[x][y] == 0 && outside_air[x][y] == 0) {
+					outside_air[x][y] = 1;
+					visit_outside(x, y, N, M, cheese, outside_air);
+				}
+		}
+	}
+
+	static void melt_cheese(int N, int M, int cheese[][], int outside_air[][]) {
+		int cheese_copy[][] = new int[N][M];
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				if (cheese[i][j] != 0) {
+					int air_count = 0;
 					for (int k = 0; k < 4; k++) {
 						int x = i + dx[k];
 						int y = j + dy[k];
-						if (0 <= x && x < R && 0 <= y && y < C && map[x][y] != -1) {
-							map2[x][y] += map[i][j] / 5;
-							sum += map[i][j] / 5;
-						}
+						if (0 <= x && x < N && 0 <= y && y < M && cheese[x][y] == 0 && outside_air[x][y] == 1)
+							air_count++;
 					}
-					map2[i][j] -= sum;
+					if (air_count >= 2)
+						cheese_copy[i][j] = -1;
 				}
-			}
-		for (int i = 0; i < R; i++)
-			for (int j = 0; j < C; j++)
-				map[i][j] += map2[i][j];
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				cheese[i][j] += cheese_copy[i][j];
 	}
 
-	static void operate_air_purifier(int R, int C, int map[][], int air_purifier) {
-		for (int i = air_purifier - 2; i > 0; i--)
-			map[i][0] = map[i - 1][0];
-		for (int i = air_purifier + 1; i < R - 1; i++)
-			map[i][0] = map[i + 1][0];
-		for (int i = 0; i < C - 1; i++) {
-			map[0][i] = map[0][i + 1];
-			map[R - 1][i] = map[R - 1][i + 1];
-		}
-		for (int i = R - 1; i > air_purifier; i--)
-			map[i][C - 1] = map[i - 1][C - 1];
-		for (int i = 0; i < air_purifier - 1; i++)
-			map[i][C - 1] = map[i + 1][C - 1];
-		for (int i = C - 1; i > 1; i--) {
-			map[air_purifier - 1][i] = map[air_purifier - 1][i - 1];
-			map[air_purifier][i] = map[air_purifier][i - 1];
-		}
-		map[air_purifier - 1][1] = 0;
-		map[air_purifier][1] = 0;
+	static boolean check(int cheese[][], int N, int M) {
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				if (cheese[i][j] == 1)
+					return false;
+		return true;
 	}
 }
