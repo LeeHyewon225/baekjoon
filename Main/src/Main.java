3725a46f067ed0093f61f8904614f547ffa78643
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-20 16:08:40 +0900

    1325번 - 효율적인 해킹

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,39 +7,37 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int max_list[];
+	static int max = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken()) + 1;
-		int X = Integer.parseInt(st.nextToken()) - 1;
 		ArrayList<Integer> A[] = new ArrayList[N];
+		max_list = new int[N];
 		for (int i = 0; i < N; i++)
 			A[i] = new ArrayList<Integer>();
-		int visit[] = new int[N];
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken()) - 1;
 			int b = Integer.parseInt(st.nextToken()) - 1;
-			A[a].add(b);
+			A[b].add(a);
 		}
-		BFS(X, N, A, visit);
-		boolean result = false;
-		for (int i = 0; i < N; i++) 
-			if (visit[i] == K) {
-				result = true;
-				System.out.println(i + 1);
-			}
-		if (!result)
-			System.out.println(-1);
+		for (int i = 0; i < N; i++)
+			BFS(i, N, A);
+		for (int i = 0; i < N; i++)
+			if (max == max_list[i])
+				System.out.print(i + 1 + " ");
 	}
 
-	static void BFS(int n, int N, ArrayList<Integer> A[], int visit[]) {
+	static void BFS(int n, int N, ArrayList<Integer> A[]) {
+		int visit[] = new int[N];
 		Queue<Integer> q = new LinkedList<Integer>();
 		visit[n] = 1;
 		q.add(n);
+		int count = 0;
 		while (!q.isEmpty()) {
 			int now = q.poll();
 			for (int i = 0; i < A[now].size(); i++) {
@@ -49,6 +47,9 @@ public class Main {
 					q.add(a);
 				}
 			}
+			count++;
 		}
+		max_list[n] = count;
+		max = Math.max(max, max_list[n]);
 	}
 }
