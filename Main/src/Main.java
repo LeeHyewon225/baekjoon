a0d0da3697bd5d8430a879ac6643deb55183f2e0
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-22 15:57:20 +0900

    1043번 - 거짓말(유니온 파인드)

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -8,28 +9,42 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		Node = new int[n + 1];
 		for (int i = 1; i <= n; i++)
 			Node[i] = i;
-		for (int i = 1; i <= n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= n; j++)
-				if (Integer.parseInt(st.nextToken()) == 1)
-					union(i, j);
-		}
 		st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		for (int i = 1; i < m; i++) {
-			int b = Integer.parseInt(st.nextToken());
-			if (find(a) != find(b)) {
-				System.out.println("NO");
-				return;
-			}
+		int true_people_count = Integer.parseInt(st.nextToken());
+		int true_people[] = new int[true_people_count];
+		for (int i = 0; i < true_people_count; i++)
+			true_people[i] = Integer.parseInt(st.nextToken());
+		ArrayList<Integer> party[] = new ArrayList[m + 1];
+		for (int i = 1; i <= m; i++) {
+			party[i] = new ArrayList<Integer>();
+			st = new StringTokenizer(br.readLine());
+			int t = Integer.parseInt(st.nextToken());
+			for (int j = 1; j <= t; j++)
+				party[i].add(Integer.parseInt(st.nextToken()));
 		}
-		System.out.println("YES");
+		for (int i = 1; i <= m; i++) {
+			int first = party[i].get(0);
+			for (int j = 1; j < party[i].size(); j++)
+				union(first, party[i].get(j));
+		}
+		int result = 0;
+		for (int i = 1; i <= m; i++) {
+			boolean possible = true;
+			for (int j = 0; j < true_people_count; j++)
+				if (find(party[i].get(0)) == find(true_people[j])) {
+					possible = false;
+					break;
+				}
+			if (possible)
+				result++;
+		}
+		System.out.println(result);
 	}
 
 	static void union(int a, int b) {
