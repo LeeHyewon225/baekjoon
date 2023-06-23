6fbb0cdd0bf54699432e427d76d480764f320822
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-23 17:47:41 +0900

    1948번 - 임계경로

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -10,40 +10,66 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
-		int time[] = new int[n];
+		ArrayList<rode> A[] = new ArrayList[n];
+		ArrayList<rode> reverse_A[] = new ArrayList[n];
 		int result[] = new int[n];
-		ArrayList<Integer> s[] = new ArrayList[n];
-		int d[] = new int[n];
-		for (int i = 0; i < n; i++)
-			s[i] = new ArrayList<Integer>();
+		int indegree[] = new int[n];
+		int visit[] = new int[n];
 		for (int i = 0; i < n; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			time[i] = Integer.parseInt(st.nextToken());
-			while (true) {
-				int node = Integer.parseInt(st.nextToken()) - 1;
-				if (node == -2)
-					break;
-				s[node].add(i);
-				d[i]++;
-			}
+			A[i] = new ArrayList<rode>();
+			reverse_A[i] = new ArrayList<rode>();
 		}
-		Queue<Integer> q = new LinkedList<Integer>();
-		for (int i = 0; i < n; i++)
-			if (d[i] == 0) {
-				q.add(i);
-				result[i] = time[i];
-			}
+		int m = Integer.parseInt(br.readLine());
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			int d = Integer.parseInt(st.nextToken());
+			A[a].add(new rode(b, d));
+			indegree[b]++;
+			reverse_A[b].add(new rode(a, d));
+		}
+		st = new StringTokenizer(br.readLine());
+		int start_city = Integer.parseInt(st.nextToken()) - 1;
+		int end_city = Integer.parseInt(st.nextToken()) - 1;
+		Queue<rode> q = new LinkedList<rode>();
+		q.add(new rode(start_city, 0));
 		while (!q.isEmpty()) {
-			int now = q.poll();
-			for (int i : s[now]) {
-				d[i]--;
-				result[i] = Math.max(result[i], result[now] + time[i]);
-				if (d[i] == 0) 
-					q.add(i);
+			rode now = q.poll();
+			for (rode i : A[now.city]) {
+				indegree[i.city]--;
+				result[i.city] = Math.max(result[i.city], result[now.city] + i.d);
+				if (indegree[i.city] == 0)
+					q.add(new rode(i.city, result[i.city]));
 			}
 		}
-		for (int i = 0; i < n; i++)
-			System.out.println(result[i]);
+		System.out.println(result[end_city]);
+		visit[end_city] = 1;
+		int count = 0;
+		q.add(new rode(end_city, 0));
+		while (!q.isEmpty()) {
+			rode now = q.poll();
+			for (rode i : reverse_A[now.city]) {
+				if (result[now.city] == result[i.city] + i.d) {
+					count++;
+					if (visit[i.city] == 0)
+						q.add(new rode(i.city, result[i.city]));
+					visit[i.city] = 1;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+
+	static class rode {
+		int city;
+		int d;
+
+		public rode(int city, int d) {
+			this.city = city;
+			this.d = d;
+		}
 	}
 }
