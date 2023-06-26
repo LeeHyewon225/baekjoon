d4599409fcf6decf38e0004c30f12b7baede8306
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-26 18:57:57 +0900

    11657번 - 타임머신

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,63 +1,54 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		ArrayList<Node> A[] = new ArrayList[n];
-		PriorityQueue<Integer> distance[] = new PriorityQueue[n];
-		for (int i = 0; i < n; i++) {
-			A[i] = new ArrayList<Node>();
-			distance[i] = new PriorityQueue<Integer>(k, Collections.reverseOrder());
-		}
-		for (int i = 0; i < m; i++) {
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Edge edge[] = new Edge[M];
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()) - 1;
-			int b = Integer.parseInt(st.nextToken()) - 1;
-			int v = Integer.parseInt(st.nextToken());
-			A[a].add(new Node(b, v));
+			int A = Integer.parseInt(st.nextToken()) - 1;
+			int B = Integer.parseInt(st.nextToken()) - 1;
+			int V = Integer.parseInt(st.nextToken());
+			edge[i] = new Edge(A, B, V);
 		}
-		PriorityQueue<Node> queue = new PriorityQueue<Node>();
-		queue.add(new Node(0, 0));
-		distance[0].add(0);
-		while (!queue.isEmpty()) {
-			Node now = queue.poll();
-			for (Node node : A[now.node])
-				if (distance[node.node].size() < k) {
-					distance[node.node].add(node.value + now.value);
-					queue.add(new Node(node.node, node.value + now.value));
-				} else if (distance[node.node].peek() > node.value + now.value) {
-					distance[node.node].poll();
-					distance[node.node].add(node.value + now.value);
-					queue.add(new Node(node.node, node.value + now.value));
-				}
-		}
-		for (int i = 0; i < n; i++)
-			System.out.println(distance[i].size() == k ? distance[i].poll() : -1);
+		long distance[] = new long[N];
+		for (int i = 0; i < distance.length; i++)
+			distance[i] = Long.MAX_VALUE;
+		distance[0] = 0;
+		for (int i = 0; i < N - 1; i++)
+			for (int j = 0; j < M; j++)
+				if (distance[edge[j].start_node] != Long.MAX_VALUE
+						&& distance[edge[j].end_node] > distance[edge[j].start_node] + edge[j].value)
+					distance[edge[j].end_node] = distance[edge[j].start_node] + edge[j].value;
+		for (int j = 0; j < M; j++)
+			if (distance[edge[j].start_node] != Long.MAX_VALUE
+					&& distance[edge[j].end_node] > distance[edge[j].start_node] + edge[j].value) {
+				System.out.println(-1);
+				return;
+			}
+		for (int i = 1; i < distance.length; i++)
+			System.out.println(distance[i] == Long.MAX_VALUE ? -1 : distance[i]);
 	}
 
-	static class Node implements Comparable<Node> {
-		public int node;
+	static class Edge {
+		public int start_node;
+		public int end_node;
 		public int value;
 
-		public Node(int node, int value) {
-			this.node = node;
+		public Edge(int start_node, int end_node, int value) {
+			this.start_node = start_node;
+			this.end_node = end_node;
 			this.value = value;
-		}
-
-		@Override
-		public int compareTo(Node o) {
-			return this.value - o.value;
 		}
 	}
 }
