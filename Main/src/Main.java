e002ee74b52a542531d1b191c548205e0a5803bd
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-26 16:36:32 +0900

    1753번 - 최단경로

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,74 +2,61 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int n = Integer.parseInt(br.readLine());
-		ArrayList<rode> A[] = new ArrayList[n];
-		ArrayList<rode> reverse_A[] = new ArrayList[n];
-		int result[] = new int[n];
-		int indegree[] = new int[n];
-		int visit[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			A[i] = new ArrayList<rode>();
-			reverse_A[i] = new ArrayList<rode>();
-		}
-		int m = Integer.parseInt(br.readLine());
-		for (int i = 0; i < m; i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(br.readLine()) - 1;
+		ArrayList<Node> A[] = new ArrayList[V];
+		for (int i = 0; i < V; i++)
+			A[i] = new ArrayList<Node>();
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()) - 1;
-			int b = Integer.parseInt(st.nextToken()) - 1;
-			int d = Integer.parseInt(st.nextToken());
-			A[a].add(new rode(b, d));
-			indegree[b]++;
-			reverse_A[b].add(new rode(a, d));
+			int u = Integer.parseInt(st.nextToken()) - 1;
+			int v = Integer.parseInt(st.nextToken()) - 1;
+			int w = Integer.parseInt(st.nextToken());
+			A[u].add(new Node(v, w));
 		}
-		st = new StringTokenizer(br.readLine());
-		int start_city = Integer.parseInt(st.nextToken()) - 1;
-		int end_city = Integer.parseInt(st.nextToken()) - 1;
-		Queue<rode> q = new LinkedList<rode>();
-		q.add(new rode(start_city, 0));
-		while (!q.isEmpty()) {
-			rode now = q.poll();
-			for (rode i : A[now.city]) {
-				indegree[i.city]--;
-				result[i.city] = Math.max(result[i.city], result[now.city] + i.d);
-				if (indegree[i.city] == 0)
-					q.add(new rode(i.city, result[i.city]));
-			}
-		}
-		System.out.println(result[end_city]);
-		visit[end_city] = 1;
-		int count = 0;
-		q.add(new rode(end_city, 0));
-		while (!q.isEmpty()) {
-			rode now = q.poll();
-			for (rode i : reverse_A[now.city]) {
-				if (result[now.city] == result[i.city] + i.d) {
-					count++;
-					if (visit[i.city] == 0)
-						q.add(new rode(i.city, result[i.city]));
-					visit[i.city] = 1;
+		int distance[] = new int[V];
+		int visit[] = new int[V];
+		for (int i = 0; i < V; i++)
+			distance[i] = Integer.MAX_VALUE;
+		distance[K] = 0;
+		PriorityQueue<Node> queue = new PriorityQueue<Node>();
+		queue.add(new Node(K, 0));
+		while (!queue.isEmpty()) {
+			Node now = queue.poll();
+			if (visit[now.node] == 1)
+				continue;
+			visit[now.node] = 1;
+			for (Node node : A[now.node])
+				if (distance[node.node] > distance[now.node] + node.value) {
+					distance[node.node] = distance[now.node] + node.value;
+					queue.add(new Node(node.node, distance[node.node]));
 				}
-			}
 		}
-		System.out.println(count);
+		for (int i = 0; i < V; i++)
+			System.out.println(distance[i] == Integer.MAX_VALUE ? "INF" : distance[i]);
 	}
 
-	static class rode {
-		int city;
-		int d;
+	static class Node implements Comparable<Node> {
+		public int node;
+		public int value;
 
-		public rode(int city, int d) {
-			this.city = city;
-			this.d = d;
+		public Node(int node, int value) {
+			this.node = node;
+			this.value = value;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.value - o.value;
 		}
 	}
 }
