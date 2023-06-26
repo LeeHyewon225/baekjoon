f768ffff464f56d077e19b3eb8c6ff84a32ffc44
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-26 17:54:04 +0900

    1854번 - K번째 최단경로 찾기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
@@ -9,41 +10,40 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-		ArrayList<Node> A[] = new ArrayList[N];
-		for (int i = 0; i < N; i++)
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		ArrayList<Node> A[] = new ArrayList[n];
+		PriorityQueue<Integer> distance[] = new PriorityQueue[n];
+		for (int i = 0; i < n; i++) {
 			A[i] = new ArrayList<Node>();
-		for (int i = 0; i < M; i++) {
+			distance[i] = new PriorityQueue<Integer>(k, Collections.reverseOrder());
+		}
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken()) - 1;
 			int b = Integer.parseInt(st.nextToken()) - 1;
 			int v = Integer.parseInt(st.nextToken());
 			A[a].add(new Node(b, v));
 		}
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken()) - 1;
-		int end = Integer.parseInt(st.nextToken()) - 1;
-		int distance[] = new int[N];
-		int visit[] = new int[N];
-		for (int i = 0; i < N; i++)
-			distance[i] = Integer.MAX_VALUE;
-		distance[start] = 0;
 		PriorityQueue<Node> queue = new PriorityQueue<Node>();
-		queue.add(new Node(start, 0));
+		queue.add(new Node(0, 0));
+		distance[0].add(0);
 		while (!queue.isEmpty()) {
 			Node now = queue.poll();
-			if (visit[now.node] == 1)
-				continue;
-			visit[now.node] = 1;
 			for (Node node : A[now.node])
-				if (distance[node.node] > distance[now.node] + node.value) {
-					distance[node.node] = distance[now.node] + node.value;
-					queue.add(new Node(node.node, distance[node.node]));
+				if (distance[node.node].size() < k) {
+					distance[node.node].add(node.value + now.value);
+					queue.add(new Node(node.node, node.value + now.value));
+				} else if (distance[node.node].peek() > node.value + now.value) {
+					distance[node.node].poll();
+					distance[node.node].add(node.value + now.value);
+					queue.add(new Node(node.node, node.value + now.value));
 				}
 		}
-		System.out.println(distance[end]);
+		for (int i = 0; i < n; i++)
+			System.out.println(distance[i].size() == k ? distance[i].poll() : -1);
 	}
 
 	static class Node implements Comparable<Node> {
