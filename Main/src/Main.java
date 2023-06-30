0fb4ed08eff1d7b1103bb7c3f7b3df34dd8e9438
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-30 17:41:22 +0900

    11403번 - 경로 찾기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -9,27 +9,21 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
 		int distance[][] = new int[n][n];
-		for (int i = 0; i < distance.length; i++)
-			for (int j = 0; j < distance.length; j++)
-				distance[i][j] = i == j ? 0 : 100000000;
-		for (int i = 0; i < m; i++) {
+		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken()) - 1;
-			int e = Integer.parseInt(st.nextToken()) - 1;
-			int v = Integer.parseInt(st.nextToken());
-			if (distance[s][e] > v)
-				distance[s][e] = v;
+			for (int j = 0; j < n; j++) {
+				distance[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
 		for (int k = 0; k < n; k++)
 			for (int i = 0; i < n; i++)
 				for (int j = 0; j < n; j++)
-					if (distance[i][j] > distance[i][k] + distance[k][j])
-						distance[i][j] = distance[i][k] + distance[k][j];
+					if (distance[i][k] == 1 && distance[k][j] == 1)
+						distance[i][j] = 1;
 		for (int i = 0; i < distance.length; i++) {
 			for (int j = 0; j < distance.length; j++)
-				System.out.print(distance[i][j] == 100000000 ? 0 + " " : distance[i][j] + " ");
+				System.out.print(distance[i][j] + " ");
 			System.out.println();
 		}
 	}
