bb1e5c481831517cafdd6c1f6558a585989bca80
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-30 18:01:53 +0900

    1389번 - 케빈 베이컨의 6단계 법칙

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,24 +7,36 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 		int distance[][] = new int[n][n];
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < distance.length; i++) 
+			for (int j = 0; j < distance.length; j++) 
+				distance[i][j] = i == j ? 0 : 1000000;
+		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
-				distance[i][j] = Integer.parseInt(st.nextToken());
-			}
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
+			distance[a][b] = 1;
+			distance[b][a] = 1;
 		}
 		for (int k = 0; k < n; k++)
 			for (int i = 0; i < n; i++)
 				for (int j = 0; j < n; j++)
-					if (distance[i][k] == 1 && distance[k][j] == 1)
-						distance[i][j] = 1;
+					if (distance[i][j] > distance[i][k] + distance[k][j])
+						distance[i][j] = distance[i][k] + distance[k][j];
+		int min = 100000;
+		int min_index = -1;
 		for (int i = 0; i < distance.length; i++) {
+			int sum = 0;
 			for (int j = 0; j < distance.length; j++)
-				System.out.print(distance[i][j] + " ");
-			System.out.println();
+				sum += distance[i][j];
+			if (sum < min) {
+				min = sum;
+				min_index = i;
+			}
 		}
+		System.out.println(min_index + 1);
 	}
 }
