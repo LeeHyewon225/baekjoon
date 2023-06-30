9f92092071f7270883f3846b09d123e6fc038dbb
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-06-30 18:55:05 +0900

    1806번 - 부분합

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -9,34 +9,36 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int distance[][] = new int[n][n];
-		for (int i = 0; i < distance.length; i++) 
-			for (int j = 0; j < distance.length; j++) 
-				distance[i][j] = i == j ? 0 : 1000000;
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken()) - 1;
-			int b = Integer.parseInt(st.nextToken()) - 1;
-			distance[a][b] = 1;
-			distance[b][a] = 1;
+		int s = Integer.parseInt(st.nextToken());
+		int a[] = new int[n];
+		st = new StringTokenizer(br.readLine());
+		int zero = 0;
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+			zero += a[i];
 		}
-		for (int k = 0; k < n; k++)
-			for (int i = 0; i < n; i++)
-				for (int j = 0; j < n; j++)
-					if (distance[i][j] > distance[i][k] + distance[k][j])
-						distance[i][j] = distance[i][k] + distance[k][j];
-		int min = 100000;
-		int min_index = -1;
-		for (int i = 0; i < distance.length; i++) {
-			int sum = 0;
-			for (int j = 0; j < distance.length; j++)
-				sum += distance[i][j];
-			if (sum < min) {
-				min = sum;
-				min_index = i;
+		if (zero < s) {
+			System.out.println(0);
+			return;
+		}
+		int min = n;
+		int count = 1;
+		int start = 0;
+		int end = 0;
+		int sum = a[0];
+		while (start <= end) {
+			if (sum < s) {
+				if (end == n - 1)
+					break;
+				sum += a[++end];
+				count++;
+			} else if (sum >= s) {
+				if (min > count)
+					min = count;
+				sum -= a[start++];
+				count--;
 			}
 		}
-		System.out.println(min_index + 1);
+		System.out.println(min);
 	}
 }
