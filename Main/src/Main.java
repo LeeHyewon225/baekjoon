f19bb624714f815ea770e8dfb0118ccd7fc952c7
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-02 21:53:09 +0900

    1197번 - 최소 스패닝 트리

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,116 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int row[] = { -1, 0, 1, 0 };
-	static int col[] = { 0, 1, 0, -1 };
-	static int answer = 11;
-	static int O_row;
-	static int O_col;
-	static int N;
-	static int M;
-	static char map[][];
+	static int parent[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new char[N][M];
-		int R_row = 0;
-		int B_row = 0;
-		int R_col = 0;
-		int B_col = 0;
-		for (int i = 0; i < N; i++) {
-			map[i] = br.readLine().toCharArray();
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 'R') {
-					R_row = i;
-					R_col = j;
-					map[i][j] = '.';
-				} else if (map[i][j] == 'B') {
-					B_row = i;
-					B_col = j;
-					map[i][j] = '.';
-				} else if (map[i][j] == 'O') {
-					O_row = i;
-					O_col = j;
-				}
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		parent = new int[V + 1];
+		for (int i = 1; i <= V; i++)
+			parent[i] = i;
+		Edge edge[] = new Edge[E];
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			edge[i] = new Edge(a, b, c);
+		}
+		Arrays.sort(edge);
+		int sum = 0;
+		int count = 0;
+		for (int i = 0; i < E; i++) {
+			if (count == V - 1)
+				break;
+			Edge e = edge[i];
+			if (find(e.node1) != find(e.node2)) {
+				union(e.node1, e.node2);
+				sum += e.value;
+				count++;
 			}
 		}
-		DFS(R_row, R_col, B_row, B_col, 0, 1);
-		DFS(R_row, R_col, B_row, B_col, 1, 1);
-		DFS(R_row, R_col, B_row, B_col, 2, 1);
-		DFS(R_row, R_col, B_row, B_col, 3, 1);
-		System.out.println(answer == 11 ? -1 : answer);
+		System.out.println(sum);
 	}
 
-	static void DFS(int R_row, int R_col, int B_row, int B_col, int direction, int count) {
-		if (answer <= count)
-			return;
-		if (R_row < B_row && direction == 0 || R_col > B_col && direction == 1 || R_row > B_row && direction == 2
-				|| R_col < B_col && direction == 3) {
-			while (map[R_row + row[direction]][R_col + col[direction]] == '.') {
-				if (R_row + row[direction] == B_row && R_col + col[direction] == B_col)
-					break;
-				R_row += row[direction];
-				R_col += col[direction];
-			}
-			if (R_row + row[direction] == O_row && R_col + col[direction] == O_col) {
-				R_row += row[direction];
-				R_col += col[direction];
-			}
-			while (map[B_row + row[direction]][B_col + col[direction]] == '.') {
-				if (B_row + row[direction] == R_row && B_col + col[direction] == R_col) {
-					break;
-				}
-				B_row += row[direction];
-				B_col += col[direction];
-			}
-			if (B_row + row[direction] == O_row && B_col + col[direction] == O_col) {
-				B_row += row[direction];
-				B_col += col[direction];
-			}
-		} else {
-			while (map[B_row + row[direction]][B_col + col[direction]] == '.') {
-				if (B_row + row[direction] == R_row && B_col + col[direction] == R_col)
-					break;
-				B_row += row[direction];
-				B_col += col[direction];
-			}
-			if (B_row + row[direction] == O_row && B_col + col[direction] == O_col) {
-				B_row += row[direction];
-				B_col += col[direction];
-			}
-			while (map[R_row + row[direction]][R_col + col[direction]] == '.') {
-				if (R_row + row[direction] == B_row && R_col + col[direction] == B_col)
-					break;
-				R_row += row[direction];
-				R_col += col[direction];
-			}
-			if (R_row + row[direction] == O_row && R_col + col[direction] == O_col) {
-				R_row += row[direction];
-				R_col += col[direction];
-			}
+	static void union(int a, int b) {
+		parent[find(b)] = find(a);
+	}
 
-		}
-		if (B_row == O_row && B_col == O_col) {
-			return;
-		}
-		if (R_row == O_row && R_col == O_col) {
-			if (answer > count)
-				answer = count;
-			return;
-		}
-		if (direction != 0)
-			DFS(R_row, R_col, B_row, B_col, 0, count + 1);
-		if (direction != 1)
-			DFS(R_row, R_col, B_row, B_col, 1, count + 1);
-		if (direction != 2)
-			DFS(R_row, R_col, B_row, B_col, 2, count + 1);
-		if (direction != 3)
-			DFS(R_row, R_col, B_row, B_col, 3, count + 1);
+	static int find(int a) {
+		if (a == parent[a])
+			return a;
+		else
+			return parent[a] = find(parent[a]);
+	}
+}
+
+class Edge implements Comparable<Edge> {
+	int node1;
+	int node2;
+	int value;
+
+	public Edge(int node1, int node2, int value) {
+		this.node1 = node1;
+		this.node2 = node2;
+		this.value = value;
+	}
+
+	public int compareTo(Edge e) {
+		return this.value - e.value;
 	}
 }
