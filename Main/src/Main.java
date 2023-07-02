9187ac48b90570d357c0031f9c1abe22d868146a
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-02 22:58:55 +0900

    17472번 - 다리 만들기 2

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,42 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int map[][];
 	static int parent[];
+	static PriorityQueue<Edge> q = new PriorityQueue<Edge>();
+	static int dx[] = { -1, 0, 1, 0 };
+	static int dy[] = { 0, 1, 0, -1 };
+	static int N;
+	static int M;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		parent = new int[V + 1];
-		for (int i = 1; i <= V; i++)
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		parent = new int[8];
+		for (int i = 1; i <= 7; i++)
 			parent[i] = i;
-		Edge edge[] = new Edge[E];
-		for (int i = 0; i < E; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			edge[i] = new Edge(a, b, c);
+			for (int j = 0; j < M; j++)
+				map[i][j] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(edge);
-		int sum = 0;
+		int island = 2;
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				if (map[i][j] == 1)
+					DFS(i, j, island++);
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				if (map[i][j] != 0)
+					for (int k = 0; k < 4; k++) {
+						int x = i + dx[k];
+						int y = j + dy[k];
+						if (0 <= x && x < N && 0 <= y && y < M && map[x][y] == 0)
+							is_Edge(x, y, map[i][j], k);
+					}
 		int count = 0;
-		for (int i = 0; i < E; i++) {
-			if (count == V - 1)
-				break;
-			Edge e = edge[i];
+		int sum = 0;
+		while (count < island - 2 && !q.isEmpty()) {
+			Edge e = q.poll();
 			if (find(e.node1) != find(e.node2)) {
 				union(e.node1, e.node2);
 				sum += e.value;
 				count++;
 			}
 		}
-		System.out.println(sum);
+		System.out.println(count == island - 3 ? sum : -1);
+	}
+
+	static void is_Edge(int i, int j, int node, int k) {
+		int count = 0;
+		while (map[i][j] == 0) {
+			count++;
+			i += dx[k];
+			j += dy[k];
+			if (0 > i || i >= N || 0 > j || j >= M) {
+				count = 0;
+				break;
+			}
+		}
+		if (count >= 2)
+			q.add(new Edge(node, map[i][j], count));
+	}
+
+	static void DFS(int i, int j, int island) {
+		map[i][j] = island;
+		for (int k = 0; k < 4; k++) {
+			int x = i + dx[k];
+			int y = j + dy[k];
+			if (0 <= x && x < N && 0 <= y && y < M && map[x][y] == 1)
+				DFS(x, y, island);
+		}
 	}
 
 	static void union(int a, int b) {
