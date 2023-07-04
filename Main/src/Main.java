96445d025f4e26d61a33410a5c8e85cbd4e59198
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-04 17:14:11 +0900

    11725번 - 트리의 부모 찾기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,71 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int parent[];
+	static int result[];
+	static int visit[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		char computer[][] = new char[N][N];
-		PriorityQueue<Edge> q = new PriorityQueue<Edge>();
-		parent = new int[N + 1];
-		for (int i = 1; i <= N; i++)
-			parent[i] = i;
-		int all = 0;
-		for (int i = 0; i < N; i++) {
-			computer[i] = br.readLine().toCharArray();
-			for (int j = 0; j < N; j++) 
-				if (computer[i][j] != '0') {
-					int value;
-					if (computer[i][j] >= 'a')
-						value = computer[i][j] - 'a' + 1;
-					else
-						value = computer[i][j] - 'A' + 27;
-					all += value;
-					if (i != j)
-						q.add(new Edge(i, j, value));
+		int n = Integer.parseInt(br.readLine());
+		ArrayList<Integer> a[] = new ArrayList[n + 1];
+		result = new int[n + 1];
+		visit = new int[n + 1];
+		visit[1] = 1;
+		for (int i = 0; i < a.length; i++)
+			a[i] = new ArrayList<Integer>();
+		for (int i = 0; i < n - 1; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int q = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			a[q].add(b);
+			a[b].add(q);
+		}
+		BFS(1, a);
+		for (int i = 2; i < result.length; i++)
+			System.out.println(result[i]);
+	}
+
+	static void BFS(int n, ArrayList<Integer> a[]) {
+		Queue<Integer> q = new LinkedList<Integer>();
+		q.add(n);
+		while (!q.isEmpty()) {
+			int now = q.poll();
+			for (int next : a[now])
+				if (visit[next] == 0) {
+					visit[next] = 1;
+					result[next] = now;
+					q.add(next);
 				}
 		}
-		int sum = 0;
-		int count = 0;
-		while (count < N - 1 && !q.isEmpty()) {
-			Edge e = q.poll();
-			if (find(e.node1) != find(e.node2)) {
-				union(e.node1, e.node2);
-				sum += e.value;
-				count++;
-			}
-		}
-		System.out.println(count == N - 1 ? all - sum : -1);
-	}
-
-	static void union(int a, int b) {
-		parent[find(b)] = find(a);
-	}
-
-	static int find(int a) {
-		if (a == parent[a])
-			return a;
-		else
-			return parent[a] = find(parent[a]);
-	}
-}
-
-class Edge implements Comparable<Edge> {
-	int node1;
-	int node2;
-	int value;
-
-	public Edge(int node1, int node2, int value) {
-		this.node1 = node1;
-		this.node2 = node2;
-		this.value = value;
-	}
-
-	public int compareTo(Edge e) {
-		return this.value - e.value;
 	}
 }
