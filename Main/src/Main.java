b49a48ad9674015ecbcb5f05e442ced2189442b6
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-04 18:58:03 +0900

    1991번 - 트리 순회

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,44 +4,48 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int visit[];
-	static int answer = 0;
+	static int tree[][];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		tNode root = new tNode();
+		int n = Integer.parseInt(br.readLine());
+		tree = new int[26][2];
 		for (int i = 0; i < n; i++) {
-			String str = br.readLine();
-			tNode now = root;
-			for (int j = 0; j < str.length(); j++) {
-				char c = str.charAt(j);
-				if (now.next[c - 'a'] == null)
-					now.next[c - 'a'] = new tNode();
-				now = now.next[c - 'a'];
-			}
-			now.isEnd = true;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int root = st.nextToken().charAt(0) - 'A';
+			char l = st.nextToken().charAt(0);
+			char r = st.nextToken().charAt(0);
+			tree[root][0] = l == '.' ? -1 : l;
+			tree[root][1] = r == '.' ? -1 : r;
 		}
-		int count = 0;
-		for (int i = 0; i < m; i++) {
-			String str = br.readLine();
-			tNode now = root;
-			for (int j = 0; j < str.length(); j++) {
-				char c = str.charAt(j);
-				if (now.next[c - 'a'] == null)
-					break;
-				now = now.next[c - 'a'];
-				if (j == str.length() - 1 && now.isEnd)
-					count++;
-			}
-		}
-		System.out.println(count);
+		preOrder('A');
+		System.out.println();
+		inOrder('A');
+		System.out.println();
+		postOrder('A');
 	}
 
-	static class tNode {
-		public tNode next[] = new tNode[26];
-		public boolean isEnd;
+	static void preOrder(int n) {
+		System.out.print((char) n);
+		if (tree[n - 'A'][0] != -1)
+			preOrder(tree[n - 'A'][0]);
+		if (tree[n - 'A'][1] != -1)
+			preOrder(tree[n - 'A'][1]);
+	}
+
+	static void inOrder(int n) {
+		if (tree[n - 'A'][0] != -1)
+			inOrder(tree[n - 'A'][0]);
+		System.out.print((char) n);
+		if (tree[n - 'A'][1] != -1)
+			inOrder(tree[n - 'A'][1]);
+	}
+
+	static void postOrder(int n) {
+		if (tree[n - 'A'][0] != -1)
+			postOrder(tree[n - 'A'][0]);
+		if (tree[n - 'A'][1] != -1)
+			postOrder(tree[n - 'A'][1]);
+		System.out.print((char) n);
 	}
 }
