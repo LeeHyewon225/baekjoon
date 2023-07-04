1cafc854f64d5ef3c0d58827459ce975426351b8
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-04 17:55:23 +0900

    1068번 - 트리

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,41 +7,51 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int result[];
 	static int visit[];
+	static int answer = 0;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		ArrayList<Integer> a[] = new ArrayList[n + 1];
-		result = new int[n + 1];
-		visit = new int[n + 1];
-		visit[1] = 1;
+		visit = new int[n];
+		int root = -1;
 		for (int i = 0; i < a.length; i++)
 			a[i] = new ArrayList<Integer>();
-		for (int i = 0; i < n - 1; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
 			int q = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			a[q].add(b);
-			a[b].add(q);
+			if (q == -1)
+				root = i;
+			else
+				a[q].add(i);
 		}
-		BFS(1, a);
-		for (int i = 2; i < result.length; i++)
-			System.out.println(result[i]);
+		int d = Integer.parseInt(br.readLine());
+		if (d == root) {
+			System.out.println(0);
+			return;
+		}
+		BFS(root, a, d);
+		System.out.println(answer);
 	}
 
-	static void BFS(int n, ArrayList<Integer> a[]) {
+	static void BFS(int n, ArrayList<Integer> a[], int d) {
 		Queue<Integer> q = new LinkedList<Integer>();
 		q.add(n);
 		while (!q.isEmpty()) {
 			int now = q.poll();
-			for (int next : a[now])
+			int count = 0;
+			for (int next : a[now]) {
+				if (next == d)
+					continue;
 				if (visit[next] == 0) {
 					visit[next] = 1;
-					result[next] = now;
 					q.add(next);
 				}
+				count++;
+			}
+			if (count == 0)
+				answer++;
 		}
 	}
 }
