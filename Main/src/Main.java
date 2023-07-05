cce49c47cc9d460af3f284473dc51bba354726ed
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-06 01:01:22 +0900

    11437번 - LCA

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,65 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	static long segment_tree[];
-	static int n;
+	static ArrayList<Integer> A[];
+	static int visit[];
+	static int parent[];
+	static int level[];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		n = 2;
-		while (n < N)
-			n *= 2;
-		n *= 2;
-		segment_tree = new long[n];
-		int index = n / 2;
-		for (int i = 0; i < N; i++)
-			segment_tree[index++] = Long.parseLong(br.readLine());
-		for (int i = n / 2 - 1; i > 0; i--)
-			segment_tree[i] = (segment_tree[i * 2] * segment_tree[i * 2 + 1]) % 1000000007;
-		for (int i = 0; i < M + K; i++) {
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		A = new ArrayList[N + 1];
+		visit = new int[N + 1];
+		parent = new int[N + 1];
+		level = new int[N + 1];
+		for (int i = 0; i < A.length; i++)
+			A[i] = new ArrayList<Integer>();
+		for (int i = 0; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
-			int t = Integer.parseInt(st.nextToken());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			if (t == 1)
-				Change(a, b);
-			else
-				System.out.println(Prefix_Sum(a, b));
+			A[a].add(b);
+			A[b].add(a);
+		}
+		visit[1] = 1;
+		DFS(1);
+		int M = Integer.parseInt(br.readLine());
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			System.out.println(LCA(a, b));
 		}
 	}
 
-	static void Change(int a, int b) {
-		a += n / 2 - 1;
-		segment_tree[a] = b;
-		a /= 2;
-		while (a >= 1) {
-			segment_tree[a] = ((segment_tree[a * 2] % 1000000007) * (segment_tree[a * 2 + 1] % 1000000007))
-					% 1000000007;
-			a /= 2;
-		}
+	static void DFS(int a) {
+		for (int i : A[a])
+			if (visit[i] == 0) {
+				visit[i] = 1;
+				parent[i] = a;
+				level[i] = level[a] + 1;
+				DFS(i);
+			}
 	}
 
-	static long Prefix_Sum(int a, int b) {
-		int start = a + n / 2 - 1;
-		int end = b + n / 2 - 1;
-		long value = 1;
-		while (start < end) {
-			if (start % 2 == 1)
-				value = value * segment_tree[start] % 1000000007;
-			if (end % 2 == 0)
-				value = value * segment_tree[end] % 1000000007;
-			start = (start + 1) / 2;
-			end = (end - 1) / 2;
+	static int LCA(int a, int b) {
+		if (level[a] < level[b]) {
+			int temp = a;
+			a = b;
+			b = temp;
 		}
-		if (start == end)
-			value = value * segment_tree[start] % 1000000007;
-		return value;
+		int n = 0;
+		int l = level[a] - level[b];
+		while (n < l) {
+			a = parent[a];
+			n++;
+		}
+		while (a != b) {
+			a = parent[a];
+			b = parent[b];
+		}
+		return a;
 	}
 }
