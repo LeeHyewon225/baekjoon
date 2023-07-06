2706dfd6b3b0bb0c81aa9fbfe06e8323a9ebcb9f
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-06 14:40:15 +0900

    14501번 - 퇴사

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,20 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
-		int dp[] = new int[N + 1];
-		for (int i = 2; i < dp.length; i++) {
-			dp[i] = dp[i - 1] + 1;
-			if (i % 2 == 0)
-				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-			if (i % 3 == 0)
-				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+		int dp[] = new int[N + 2];
+		int T[] = new int[N + 1];
+		int P[] = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(dp[N]);
+		for (int i = N; i > 0; i--) {
+			if (i + T[i] <= N + 1)
+				dp[i] = Math.max(dp[i + 1], dp[i + T[i]] + P[i]);
+			else
+				dp[i] = dp[i + 1];
+		}
+		System.out.println(dp[1]);
 	}
 }
