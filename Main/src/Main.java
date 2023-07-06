ac0acf21c0cb298c4cc8c3edeb39a21e32112132
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-06 16:20:12 +0900

    11726번 - 2×N 타일링

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,11 +7,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		long dp[] = new long[91];
+		long dp[] = new long[1001];
 		dp[1] = 1;
-		dp[2] = 1;
+		dp[2] = 2;
 		for (int i = 3; i <= N; i++)
-			dp[i] = dp[i - 1] + dp[i - 2];
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
 		System.out.println(dp[N]);
 	}
 }
