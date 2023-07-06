7ebf500e77f2e5db3abfb1ff3e0dda0ff555d89d
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-06 16:54:39 +0900

    10844번 - 쉬운 계단 수

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -7,11 +7,21 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		long dp[] = new long[1001];
-		dp[1] = 1;
-		dp[2] = 2;
-		for (int i = 3; i <= N; i++)
-			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
-		System.out.println(dp[N]);
+		long dp[][] = new long[N][10];
+		for (int i = 1; i < 10; i++) {
+			dp[0][i] = 1;
+		}
+		for (int i = 1; i < N; i++) {
+			dp[i][0] = dp[i - 1][1];
+			for (int j = 1; j < 9; j++)
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+			dp[i][9] = dp[i - 1][8];
+		}
+		long sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum += dp[N - 1][i];
+			sum %= 1000000000;
+		}
+		System.out.println(sum);
 	}
 }
