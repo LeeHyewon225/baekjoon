1c5f2befbeee5f9c5a8b357d055651cf6a904ed7
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-06 15:49:53 +0900

    2193번 - 이친수

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,28 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
-		int dp[] = new int[N + 2];
-		int T[] = new int[N + 1];
-		int P[] = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			T[i] = Integer.parseInt(st.nextToken());
-			P[i] = Integer.parseInt(st.nextToken());
-		}
-		for (int i = N; i > 0; i--) {
-			if (i + T[i] <= N + 1)
-				dp[i] = Math.max(dp[i + 1], dp[i + T[i]] + P[i]);
-			else
-				dp[i] = dp[i + 1];
-		}
-		System.out.println(dp[1]);
+		long dp[] = new long[91];
+		dp[1] = 1;
+		dp[2] = 1;
+		for (int i = 3; i <= N; i++)
+			dp[i] = dp[i - 1] + dp[i - 2];
+		System.out.println(dp[N]);
 	}
 }
