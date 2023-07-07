78e4a754e18ee36c23be01ce8b3ee9935b66ed12
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-07 22:32:52 +0900

    1915번 - 가장 큰 정사각형

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,38 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str1 = br.readLine();
-		String str2 = br.readLine();
-		int LCS[][] = new int[str1.length() + 1][str2.length() + 1];
-		for (int i = 1; i < str1.length() + 1; i++)
-			for (int j = 1; j < str2.length() + 1; j++)
-				if (str1.charAt(i - 1) != str2.charAt(j - 1))
-					LCS[i][j] = Math.max(LCS[i - 1][j], LCS[i][j - 1]);
-				else
-					LCS[i][j] = LCS[i - 1][j - 1] + 1;
-		System.out.println(LCS[str1.length()][str2.length()]);
-		if (LCS[str1.length()][str2.length()] == 0)
-			return;
-		int i = str1.length();
-		int j = str2.length();
-		StringBuffer sb = new StringBuffer();
-		while (i > 0 && j > 0) {
-			if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-				sb.append(str1.charAt(i - 1));
-				i--;
-				j--;
-			} else {
-				if (LCS[i - 1][j] < LCS[i][j - 1])
-					j--;
-				else
-					i--;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int D[][] = new int[n + 1][m + 1];
+		int max = 0;
+		for (int i = 1; i <= n; i++) {
+			String str = br.readLine();
+			for (int j = 1; j <= m; j++) {
+				char c = str.charAt(j - 1);
+				if (c == '1') {
+					D[i][j] = Math.min(Math.min(D[i - 1][j], D[i][j - 1]), D[i - 1][j - 1]) + 1;
+					max = Math.max(max, D[i][j]);
+				} else
+					D[i][j] = 0;
 			}
 		}
-		System.out.println(sb.reverse());
+		System.out.println(max * max);
 	}
 }
