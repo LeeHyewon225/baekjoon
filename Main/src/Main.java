8027b0a62c423a780bb72974c2820fb6efa90e52
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-09 20:43:34 +0900

    11049번 - 행렬 곱셈 순서

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,35 +4,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int D[][];
+	static int matrix[][];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int D[][][] = new int[100001][5][5];
-		for (int i = 0; i < 100001; i++)
-			for (int j = 0; j < 5; j++)
-				for (int k = 0; k < 5; k++)
-					D[i][j][k] = 10000000;
-		D[0][0][0] = 0;
-		int count = 1;
-		int mp[][] = { { 1, 2, 2, 2, 2 }, { -1, 1, 3, 4, 3 }, { -1, 3, 1, 3, 4 }, { -1, 4, 3, 1, 3 }, { -1, 3, 4, 3, 1 } };
-		while (true) {
-			int n = Integer.parseInt(st.nextToken());
-			if (n == 0)
-				break;
-			for (int i = 0; i < 5; i++)
-				for (int j = 0; j < 5; j++)
-					D[count][i][n] = Math.min(D[count][i][n], D[count - 1][i][j] + mp[j][n]);
-			for (int i = 0; i < 5; i++)
-				for (int j = 0; j < 5; j++)
-					D[count][n][i] = Math.min(D[count][n][i], D[count - 1][j][i] + mp[j][n]);
-			count++;
+		int N = Integer.parseInt(br.readLine());
+		matrix = new int[N][2];
+		D = new int[N][N];
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				D[i][j] = -1;
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			matrix[i][0] = Integer.parseInt(st.nextToken());
+			matrix[i][1] = Integer.parseInt(st.nextToken());
 		}
-		count--;
-		int answer = 10000000;
-		for (int i = 0; i < 5; i++)
-			for (int j = 0; j < 5; j++)
-				answer = Math.min(answer, D[count][i][j]);
-		System.out.println(answer);
+		System.out.println(dp(0, N - 1));
+	}
+
+	static int dp(int start, int end) {
+		if (D[start][end] != -1)
+			return D[start][end];
+		if (start == end)
+			return D[start][end] = 0;
+		if (start + 1 == end)
+			return D[start][end] = matrix[start][0] * matrix[end][0] * matrix[end][1];
+		int result = Integer.MAX_VALUE;
+		for (int i = start; i < end; i++)
+			result = Math.min(result, dp(start, i) + dp(i + 1, end) + matrix[start][0] * matrix[i][1] * matrix[end][1]);
+		return D[start][end] = result;
+
 	}
 }
