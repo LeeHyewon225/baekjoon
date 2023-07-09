e58817636b76e056b73a26f36d5bef7560bb76a5
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-09 23:19:12 +0900

    14003번 - 가장 긴 증가하는 부분 수열 5

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,32 +4,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int D[][];
-	static int distance[][];
-	static int N;
+	static int B[];
+	static int max_index = 1;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		distance = new int[N][N];
-		D = new int[N][1 << N];
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++)
-				distance[i][j] = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(br.readLine());
+		int S[] = new int[N + 1];
+		int D[] = new int[N + 1];
+		B = new int[N + 1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++)
+			S[i] = Integer.parseInt(st.nextToken());
+		D[1] = 1;
+		B[1] = S[1];
+		for (int i = 2; i <= N; i++)
+			D[i] = search(1, max_index, S[i]);
+		int A[] = new int[max_index + 1];
+		System.out.println(max_index);
+		for (int i = N; i > 0; i--) {
+			if (D[i] == max_index)
+				A[max_index--] = S[i];
+			if (max_index == 0)
+				break;
 		}
-		System.out.println(dp(0, 1));
+		for (int i = 1; i < A.length; i++)
+			System.out.print(A[i] + " ");
 	}
 
-	static int dp(int now, int v) {
-		if (v == (1 << N) - 1)
-			return distance[now][0] == 0 ? 100000000 : distance[now][0];
-		if (D[now][v] != 0)
-			return D[now][v];
-		int min = 100000000;
-		for (int i = 0; i < N; i++)
-			if ((v & (1 << i)) == 0 && distance[now][i] != 0)
-				min = Math.min(min, dp(i, (v | (1 << i))) + distance[now][i]);
-		return D[now][v] = min;
+	static int search(int start, int end, int n) {
+		while (start <= end) {
+			int m = (start + end) / 2;
+			if (B[m] == n)
+				return m;
+			else if (B[m] < n)
+				start = m + 1;
+			else
+				end = m - 1;
+		}
+		max_index = Math.max(max_index, start);
+		B[start] = n;
+		return start;
 	}
 }
