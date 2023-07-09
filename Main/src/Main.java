b2470a4e2066998210ed1b710fb2ed40578cbdd3
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-09 22:19:26 +0900

    2098번 - 외판원 순회

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -5,35 +5,31 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int D[][];
-	static int matrix[][];
+	static int distance[][];
+	static int N;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		matrix = new int[N][2];
-		D = new int[N][N];
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < N; j++)
-				D[i][j] = -1;
+		N = Integer.parseInt(br.readLine());
+		distance = new int[N][N];
+		D = new int[N][1 << N];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			matrix[i][0] = Integer.parseInt(st.nextToken());
-			matrix[i][1] = Integer.parseInt(st.nextToken());
+			for (int j = 0; j < N; j++)
+				distance[i][j] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(dp(0, N - 1));
+		System.out.println(dp(0, 1));
 	}
 
-	static int dp(int start, int end) {
-		if (D[start][end] != -1)
-			return D[start][end];
-		if (start == end)
-			return D[start][end] = 0;
-		if (start + 1 == end)
-			return D[start][end] = matrix[start][0] * matrix[end][0] * matrix[end][1];
-		int result = Integer.MAX_VALUE;
-		for (int i = start; i < end; i++)
-			result = Math.min(result, dp(start, i) + dp(i + 1, end) + matrix[start][0] * matrix[i][1] * matrix[end][1]);
-		return D[start][end] = result;
-
+	static int dp(int now, int v) {
+		if (v == (1 << N) - 1)
+			return distance[now][0] == 0 ? 100000000 : distance[now][0];
+		if (D[now][v] != 0)
+			return D[now][v];
+		int min = 100000000;
+		for (int i = 0; i < N; i++)
+			if ((v & (1 << i)) == 0 && distance[now][i] != 0)
+				min = Math.min(min, dp(i, (v | (1 << i))) + distance[now][i]);
+		return D[now][v] = min;
 	}
 }
