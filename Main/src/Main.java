433373510b14fc3459b475b22f0289ce4447954a
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-07-11 23:30:04 +0900

    1010번 - 다리 놓기

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,21 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int D[][] = new int[15][15];
+	static int D[][] = new int[31][31];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= 14; i++)
-			D[0][i] = i;
-		for (int i = 1; i <= 14; i++)
-			for (int j = 1; j <= 14; j++)
-				D[i][j] = D[i][j - 1] + D[i - 1][j];
+		for (int i = 1; i <= 30; i++)
+			for (int j = 1; j <= 30; j++)
+				D[i][j] = j == 1 ? i : (D[i - 1][j] + D[i - 1][j - 1]);
 		for (int t = 0; t < T; t++) {
-			int N = Integer.parseInt(br.readLine());
-			int K = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			if (N < K) {
+				int temp = N;
+				N = K;
+				K = temp;
+			}
 			System.out.println(D[N][K]);
 		}
 	}
