5a0e67a7990e2e6a9ffd3a1862bbc816fd04c495
LeeHyewon225 <78154234+LeeHyewon225@users.noreply.github.com>
2023-08-10 00:05:49 +0900

    10816번 - 숫자 카드 2

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,46 +1,45 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int a[] = new int[N + 1];
-		a[1] = 1;
-		for (int i = 2; i <= N; i++) 
-			if (a[i] == 0) 
-				for (int j = 2; i * j <= N; j++)
-					a[j * i] = 1;
-		int start = 2;
-		int end = 2;
-		int sum = 2;
-		int answer = 0;
-		while (start <= N && end <= N) 
-			if (sum == N) {
-				answer++;
-				
-				end++;
-				while (end <= N && a[end] == 1)
-					end++;
-				sum += end;
-				
-				sum -= start;
-				start++;
-				while (start <= N && a[start] == 1)
-					start++;
-			} else if (sum < N) {
-				end++;
-				while (end <= N && a[end] == 1)
-					end++;
-				sum += end;
-			} else {
-				sum -= start;
-				start++;
-				while (start <= N && a[start] == 1)
-					start++;
+		int card[] = new int[N];
+		int card_count[] = new int[20000001];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int a = Integer.parseInt(st.nextToken());
+			card[i] = a;
+			card_count[a + 10000000]++;
+		}
+		Arrays.sort(card);
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		for (int i = 0; i < M; i++) {
+			int card_num = Integer.parseInt(st.nextToken());
+			int s = 0;
+			int e = N - 1;
+			int count = 0;
+			while (s <= e) {
+				int m = (s + e) / 2;
+				if (card[m] == card_num) {
+					count = card_count[card_num + 10000000];
+					break;
+				} else if (card[m] < card_num)
+					s = m + 1;
+				else
+					e = m - 1;
 			}
-		System.out.println(answer);
+			bw.append(count + " ");
+		}
+		bw.flush();
 	}
 }
